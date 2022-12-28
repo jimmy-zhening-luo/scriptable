873f9b27846f82ba293cf5ee1ff8efb90262854c
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2022-12-27 16:00:08 -0800

    in progress: adding custom url handler class

---
+++ src/url.js
@@ -0,0 +1,27 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-blue; icon-glyph: magic;
+class Url {
+  constructor(scheme, base = String, path = String, fragment = String, queryMap = new Map) {
+    this.scheme = scheme;
+    this.base = base;
+    this.path = path;
+    this.fragment = fragment;
+    this.query = {
+      "map": queryMap,
+      "string": String
+    };
+    
+    try {
+      const queryStringFragments = Object.entries(queryMap).map(([key, value]) => String(key).concat("=", String(value)));
+      
+      this.query.string = queryStringFragments.reduce((str, substr) => String(str).concat("&", String(substr)));
+      
+    } catch (e) {
+      console.error(e);
+    }
+    
+  }
+}
+
+module.exports = Url;
