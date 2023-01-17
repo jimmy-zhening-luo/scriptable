531665a8ff0a3a9057240268233c21febf5c9b2f
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-17 15:40:57 -0800

    tiny system config field rename

--- system/System.js
+++ system/System.js
@@ -1,16 +1,18 @@
 class System {
-  static init(
-    doInstallLibrariesFromRepo = false,
-    doInstallScriptsFromRepo = false
-  ) {
-    if (
-      doInstallLibrariesFromRepo === true
-    ) this.#installLibrariesFromRepo();
-    if (doInstallScriptsFromRepo === true)
-      this.#installScriptsFromRepo();
+  static init() {
+    this.installLibraries();
+    this.installApplications();
   }
   
-  static #installLibrariesFromRepo() {
+  static installLibraries() {
+    this.#pullLibFromRepoSource();
+  }
+  
+  static installApplications() {
+    this.#pullRootFromRepoSource(false);
+  }
+  
+  static #pullLibFromRepoSource() {
     const iFm = FileManager.iCloud();
     const lFm = FileManager.local();
     const here = iFm.joinPath(
@@ -35,22 +37,14 @@ class System {
       iFm.copy(source, destination);
   }
   
-  static #installScriptsFromRepo() {
-    this.#pullScriptsFromRepo(false);
-  }
-  
-  static #pushScriptsToRepo() {
-    // TBD
-  }
-  
-  static #pullScriptsFromRepo(
+  static #pullRootFromRepoSource(
     force = false
   ) {
     if (force === true)
       pull(0);
     else {
       const confirm = new Alert();
-      confirm.message = "Initializing scripts will delete all scripts in this Are you sure you want to override current production files?";
+      confirm.message = "Initializing scripts will delete all scripts currently shown. Are you sure you want to override current production files?";
       confirm.addDestructiveAction("Yes, DELETE prod");
       confirm.addCancelAction("No, cancel");
       confirm.present().then((value) => (pull(value)));
