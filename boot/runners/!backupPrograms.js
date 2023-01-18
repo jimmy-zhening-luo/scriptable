fae61346e9915c7fdd273cff32c13a6bb510120a
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-18 15:16:03 -0800

    updating bootrunners

--- boot/runners/!backupPrograms.js
+++ boot/runners/!backupPrograms.js
@@ -2,4 +2,4 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-brown; icon-glyph: upload;
 const System = importModule("system/System");
-System.backupApplications();
+System.backupPrograms();
--- boot/runners/!installPrograms.js
+++ boot/runners/!installPrograms.js
@@ -2,4 +2,4 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: light-gray; icon-glyph: download;
 const System = importModule("system/System");
-System.installApplications();
+System.installPrograms();
