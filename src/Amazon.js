c4d85c36583a24bb21aaadf639bd26f2554d4bfc
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-17 16:17:06 -0800

    rename reference to File class in Amazon script; merge Config and Repository class into File

--- src/Amazon.js
+++ src/Amazon.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: teal; icon-glyph: magic;
-const File = importModule("lib/File");
+const File = importModule("system/File");
 
 const data = Object.freeze({
   files: {
