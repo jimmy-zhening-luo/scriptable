628bfab614a87aace3a036d87ad35a63d8911c17
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-18 13:23:34 -0800

    boot v1.0.5

--- boot/Boot.js
+++ boot/Boot.js
@@ -4,16 +4,22 @@ This file must be manually copied from the repository into the boot dir.
 When run, it loads the core system files from the repository.
 */
 
-// v1.0.3
+// v1.0.5
 
 const ROOT_BOOKMARK = "!ROOT";
+const BOOT_DIR = "boot";
 const SYSTEM_DIR = "system";
 const SYSTEM_CONFIG_FILE = "system.json";
+
 const REPO_SYSTEM_SRC_BOOKMARK = "!SYSTEM"
 
 class Boot {
   static get ROOT_BOOKMARK() {
     return ROOT_BOOKMARK;
+  }
+  
+  static get BOOT_DIR() {
+    return BOOT_DIR;
   }
   
   static get SYSTEM_DIR() {
