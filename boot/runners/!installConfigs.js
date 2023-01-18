2d16de838a84f297394de99124a8f2f69c158c83
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-18 13:11:44 -0800

    adding another bootrunner to load configd

---
+++ boot/runners/!installConfigs.js
@@ -0,0 +1,5 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: gray; icon-glyph: cog;
+const System = importModule("system/System");
+System.installConfigs();
