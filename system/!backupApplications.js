3ccd9bbbc8b7afed6cc0a579647f137395de0e9e
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-17 16:09:14 -0800

    + final bootstrapping script

---
+++ system/!backupApplications.js
@@ -0,0 +1,5 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-brown; icon-glyph: upload;
+const System = importModule("system/System");
+System.backupApplications();
