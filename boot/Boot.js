40b6288f1e864fba407e37daefca2679d4c2f095
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-18 13:11:19 -0800

    boot v1.0.4

--- boot/Boot.js
+++ boot/Boot.js
@@ -4,22 +4,44 @@ This file must be manually copied from the repository into the boot dir.
 When run, it loads the core system files from the repository.
 */
 
-// v1.0.2
+// v1.0.3
+
+const ROOT_BOOKMARK = "!ROOT";
+const SYSTEM_DIR = "system";
+const SYSTEM_CONFIG_FILE = "system.json";
+const REPO_SYSTEM_SRC_BOOKMARK = "!SYSTEM"
 
 class Boot {
+  static get ROOT_BOOKMARK() {
+    return ROOT_BOOKMARK;
+  }
+  
+  static get SYSTEM_DIR() {
+    return SYSTEM_DIR;
+  }
+  
+  static get SYSTEM_CONFIG_FILE() {
+    return SYSTEM_CONFIG_FILE;
+  }
+  
+  static get #REPO_SYSTEM_SRC_BOOKMARK() {
+    return REPO_SYSTEM_SRC_BOOKMARK;
+  }
+  
   static installSystem() {
     const iFm = FileManager.iCloud();
     const lFm = FileManager.local();
     const here = iFm.joinPath(
       iFm.bookmarkedPath(
-        "!HERE"
+        this.ROOT_BOOKMARK
       ),
-      "system"
+      this.SYSTEM_DIR
     );
     const destination = here;
       
     const there = lFm.bookmarkedPath(
-      "!SYSTEM");
+      this.#REPO_SYSTEM_SRC_BOOKMARK
+    );
     const source = there;
     
     if (iFm.isDirectory(destination))
