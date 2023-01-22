e7072c89815044bdbe1e173f9d15de8f68993ccb
Jimmy Zhening Luo <zluo@live.com>
2023-01-22 02:12:21 +0000

    stable test

---
+++ built/scriptable.js
@@ -0,0 +1,4 @@
+var _a;
+const fm = FileManager.iCloud();
+console.log((_a = fm === null || fm === void 0 ? void 0 : fm.bookmarkedPath("Yes")) !== null && _a !== void 0 ? _a : String());
+export {};
