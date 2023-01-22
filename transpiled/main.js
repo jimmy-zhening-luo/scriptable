c04e6f81b18b9cbbf453079323268bf1c6db08ba
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-21 18:09:01 -0800

    changing dist folder

--- transpiled/main.js
+++
@@ -1,7 +0,0 @@
-function hello(compiler) {
-    return ((compiler === null || compiler === void 0 ? void 0 : compiler.constructor) === String) ?
-        String(compiler)
-        : String();
-}
-console.log(hello("TypeScript"));
-export {};
--- transpiled/scriptable.js
+++
@@ -1,4 +0,0 @@
-var _a;
-const fm = FileManager.iCloud();
-console.log((_a = fm === null || fm === void 0 ? void 0 : fm.bookmarkedPath("Yes")) !== null && _a !== void 0 ? _a : String());
-export {};
