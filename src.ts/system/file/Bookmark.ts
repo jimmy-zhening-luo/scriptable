0953246eb78addd574004f172acd495b208b4456
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-21 21:23:40 -0800

    asding opt arg to bookmark

--- src.ts/system/file/Bookmark.ts
+++ src.ts/system/file/Bookmark.ts
@@ -3,12 +3,12 @@ class Bookmark {
   readonly path: string = String();
   
   constructor (
-    bookmark: string
+    bookmark: string = String()
   ) {
     this.bookmark = bookmark;
     this.path = (bookmark === String())?
       String()
-      :FileManager.iCloud().bookmarkedPath(bookmark) as string;
+      :FileManager.iCloud().bookmarkedPath(bookmark);
   }
   
   toString(): string {
