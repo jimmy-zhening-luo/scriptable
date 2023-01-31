10fc0193c52f9fe51f6c60fd1e535d79dc70e0bb
Jimmy Zhening Luo <zluo@live.com>
2023-01-31 22:30:22 +0000

    update build with new amazon glyph

--- built/Amazon.js
+++ built/Amazon.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: teal; icon-glyph: magic;
+// icon-color: orange; icon-glyph: share;
 var Amazon;
 (function (Amazon_1) {
     const shortcut = importModule("shortcut/Shortcut");
