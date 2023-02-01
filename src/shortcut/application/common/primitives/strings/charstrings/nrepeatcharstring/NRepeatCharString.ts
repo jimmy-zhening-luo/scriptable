ff514070066c8b3822135f7600d053a94566a1e6
Jimmy Zhening Luo <zluo@live.com>
2023-01-31 20:43:52 -0800

    nrepeatcharstring code complete

--- src/shortcut/application/common/primitives/strings/charstrings/nrepeatcharstring/NRepeatCharString.ts
+++ src/shortcut/application/common/primitives/strings/charstrings/nrepeatcharstring/NRepeatCharString.ts
@@ -1,12 +1,41 @@
-class NRepeatedChar extends MinMaxRepeatedChar {
+const _BoundedRepeatCharString: typeof BoundedRepeatCharString = importModule("boundedrepeatcharstring/BoundedRepeatCharString");
+
+class NRepeatCharString extends _BoundedRepeatCharString {
   constructor(
-    reps: number,
-    ...charsets: Array<RepeatedChar.RepeatedCharInput>
+    n: number,
+    charstring: string
+  );
+  constructor(
+    n: number,
+    charstring: string,
+    ...ofChars: Char[]
+  );
+  constructor(
+    n: number,
+    charstring: string,
+    ...ofStrings: string[]
+  );
+  constructor(
+    n: number,
+    charstring: string,
+    ...ofCharsets: string[][]
+  );
+  constructor(
+    n: number,
+    charstring: string,
+    ...ofCharInputs: Char.CharInput[]
+  );
+  constructor(
+    n: number,
+    charstring: string,
+    ...ofCharInputs: Char.CharInput[]
   ) {
-    super(reps, reps, ...charsets);
+    super(n, n, charstring, ...ofCharInputs);
   }
 
-  get reps() {
-    return this.minReps;
+  get n() {
+    return this.max;
   }
 }
+
+module.exports = NRepeatCharString;
