a4efad51ae471e41c793e363c935f2b033506ce9
Jimmy Zhening Luo <zluo@live.com>
2023-02-01 20:10:44 -0800

    wip

--- src/shortcut/application/browser/Url.ts
+++ src/shortcut/application/browser/Url.ts
@@ -1,10 +1,10 @@
 class Url {
-  #scheme: Scheme;
-  #host: Host;
-  #port: Port;
-  #path: Path;
-  #query: Query;
-  #fragment: Fragment;
+  #scheme: Scheme = new Url._Scheme();
+  #host: Host = new Url._Host();
+  #port: Port = new Url._Port();
+  #path: Path = new Url._Path();
+  #query: Query = new Url._Query();
+  #fragment: Fragment = new Url._Fragment();
   constructor();
   constructor(url: Url);
   constructor(urlparts: Url.UrlParts);
@@ -26,20 +26,20 @@ class Url {
     fragment?: Fragment | string
   ) {
     if (head === undefined) {
-      this.#scheme = new Url._Scheme();
-      this.#host = new Url._Host();
-      this.#port = new Url._Port();
-      this.#path = new Url._Path();
-      this.#query = new Url._Query();
-      this.#fragment = new Url._Fragment();
+      this.scheme = new Url._Scheme();
+      this.host = new Url._Host();
+      this.port = new Url._Port();
+      this.path = new Url._Path();
+      this.query = new Url._Query();
+      this.fragment = new Url._Fragment();
     }
     else if (head instanceof Url) {
-      this.#scheme = head.scheme;
-      this.#host = head.host;
-      this.#port = head.port;
-      this.#path = head.path;
-      this.#query = head.query;
-      this.#fragment = head.fragment;
+      this.scheme = head.scheme;
+      this.host = head.host;
+      this.port = head.port;
+      this.path = head.path;
+      this.query = head.query;
+      this.fragment = head.fragment;
     }
     else if (typeof head === "string"
       && host === undefined
@@ -49,39 +49,39 @@ class Url {
       && fragment === undefined
     ) {
       const parsedUrl: Url = parse(head);
-      this.#scheme = parsedUrl.scheme;
-      this.#host = parsedUrl.host;
-      this.#port = parsedUrl.port;
-      this.#path = parsedUrl.path;
-      this.#query = parsedUrl.query;
-      this.#fragment = parsedUrl.fragment;
+      this.scheme = parsedUrl.scheme;
+      this.host = parsedUrl.host;
+      this.port = parsedUrl.port;
+      this.path = parsedUrl.path;
+      this.query = parsedUrl.query;
+      this.fragment = parsedUrl.fragment;
     }
     else if (host !== undefined) {
-      this.#scheme = new Url._Scheme(head);
-      this.#host = new Url._Host(host);
-      this.#port = new Url._Port(port);
-      this.#path = new Url._Path(path);
-      this.#query = new Url._Query(query);
-      this.#fragment = new Url._Fragment(fragment);
+      this.scheme = new Url._Scheme(head);
+      this.host = new Url._Host(host);
+      this.port = new Url._Port(port);
+      this.path = new Url._Path(path);
+      this.query = new Url._Query(query);
+      this.fragment = new Url._Fragment(fragment);
     }
     else {
-      this.#scheme = new Url._Scheme(head.scheme);
-      this.#host = new Url._Host(head.host);
-      this.#port = new Url._Port(head.port);
-      this.#path = new Url._Path(head.path);
-      this.#query = new Url._Query(head.query);
-      this.#fragment = new Url._Fragment(head.fragment);
+      this.scheme = new Url._Scheme(head.scheme);
+      this.host = new Url._Host(head.host);
+      this.port = new Url._Port(head.port);
+      this.path = new Url._Path(head.path);
+      this.query = new Url._Query(head.query);
+      this.fragment = new Url._Fragment(head.fragment);
     }
-    
+
     function parse(url: string): Url {
       let urlStringParts: Url.UrlParts = { };
-      
+
       const url_fragment: string[] = url
         .trim()
         .split("#");
       url = url_fragment.shift();
       urlStringParts.fragment = url_fragment.join("#");
-      
+
       const queryOrSchemehostportpath_query: string[] = url.split("?");
       const queryOrSchemehostportpath: string = queryOrSchemehostportpath_query.shift();
       const schemehostpath: string = queryOrSchemehostportpath.includes("=") ?
@@ -93,7 +93,7 @@ class Url {
           ...queryOrSchemehostportpath_query
         ].join("?")
         : queryOrSchemehostportpath_query.join("?");
-        
+
       const scheme_hostportpath: string[] = schemehostportpath.split("://");
       const schemeOrHostportpath: string = scheme_hostportpath.shift();
       urlStringParts.scheme = scheme_hostportpath.length > 0 ?
@@ -107,20 +107,20 @@ class Url {
         : urlStringParts.scheme === "" ?
           schemeOrHostportpath
           : "";
-      
+
       const hostport_path: string[] = Url._File
         .trimPath(hostportpath)
         .split("/");
       const hostport: string = hostport_path.shift();
       urlStringParts.path = hostport_path.join("/");
-      
+
       const host_port: string[] =
         hostport.split(":");
       urlStringParts.host = host_port.shift();
       urlStringParts.port = urlStringParts.host === "" ?
         ""
         : host_port.join(":");
-        
+
       return new Url(urlStringParts);
     }
   }
@@ -230,21 +230,21 @@ class Url {
         ""
         : ["https", hostPort].join("://")
       : [this.scheme, hostPort].join("://");
-    
+
     const pathQuery: string = this.query === "" ?
       this.path
       : [this.path, this.query].join("?");
     const pathQueryFragment: string = this.fragment === "" ?
       pathQuery
       : [pathQuery, this.fragment].join("#");
-    
+
     return schemeHostPort === "" ?
       ""
       : pathQueryFragment === "" ?
         schemeHostPort
         : [schemeHostPort, pathQueryFragment].join("/");
   }
-  
+
   static encode(
     url: string
   ): string {
@@ -291,9 +291,9 @@ namespace Url {
     query?: string | Query,
     fragment?: string | Fragment
   };
-  
+
   export const _File: typeof File = importModule("./shortcut/application/appdata/filesystem/file/File");
-  
+
   export const _Scheme: typeof Scheme = importModule("urlparts/Scheme");
   export const _Host: typeof Host = importModule("urlparts/Host");
   export const _Port: typeof Port = importModule("urlparts/Port");
