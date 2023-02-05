c4be5e23f5746bd2e74bfdec150878c76b46de20
Jimmy Zhening Luo <zluo@live.com>
2023-02-05 01:02:50 -0800

    rebuilding browser section

--- built/shortcut/application/browser/urlcomposites/urlcomposite/UrlComposite.d.ts
+++
@@ -1,8 +0,0 @@
-declare abstract class UrlComposite {
-    abstract readonly parts: Array<UrlPart | UrlComposite>;
-    abstract get composite(): string;
-    get isValid(): boolean;
-    get string(): string;
-    toString(): string;
-}
-//# sourceMappingURL=UrlComposite.d.ts.map
--- built/shortcut/application/browser/urlcomposites/urlparts/repeaters/urlpartrepeater/UrlPartRepeater.d.ts
+++
@@ -1,9 +0,0 @@
-declare abstract class UrlPartRepeater {
-    readonly value: null | string;
-    constructor(repeater: string);
-    protected abstract parse(repeater: string): null | string;
-    get isValid(): boolean;
-    get string(): string;
-    toString(): string;
-}
-//# sourceMappingURL=UrlPartRepeater.d.ts.map
