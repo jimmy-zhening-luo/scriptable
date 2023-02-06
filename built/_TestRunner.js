d582563fd9bd3e4b6b6e52aa5f722063bd1b6e57
Jimmy Zhening Luo <zluo@live.com>
2023-02-06 02:23:50 +0000

    testrun skeleton built

---
+++ built/_TestRunner.js
@@ -0,0 +1,8 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: teal; icon-glyph: magic;
+var TestRunner;
+(function (TestRunner) {
+    const shortcut = importModule("system/Shortcut");
+})(TestRunner || (TestRunner = {}));
+//# sourceMappingURL=_TestRunner.js.map
