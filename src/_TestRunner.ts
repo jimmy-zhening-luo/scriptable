a48492964b48e39988f909aa24887051572d001f
Jimmy Zhening Luo <zluo@live.com>
2023-02-05 20:00:27 -0800

    added test runner class

--- src/_TestRunner.ts
+++ src/_TestRunner.ts
@@ -3,7 +3,6 @@
 // icon-color: teal; icon-glyph: magic;
 
 namespace TestRunner {
-
   
   
   
@@ -16,19 +15,126 @@ namespace TestRunner {
   
   
   
-  
-  
-  
-  
-  interface TestCase {
-    evaluate: any,
-    result: any
-  }
-  
-  interface TestSuite {
-    id: string,
-    cases: TestCase[]
-  }
-  
+  // TestRunner imports
   const shortcut: typeof Shortcut = importModule("system/Shortcut");
+  
+  
+  
+  
+  
+  
+  // TestRunner interfaces and classes
+  type Evaluate = any;
+  type Result = any;
+  type TestCase = [Evaluate, Result];
+  type TestCases = TestCase[];
+  
+  class TestSuite {
+    readonly id: string,
+    readonly cases: TestCases
+    
+    constructor(
+      id: string,
+      ...cases:
+        Array<TestCase | TestCases>
+    );
+    
+    constructor(
+      id: string,
+      testSuite?: TestSuite,
+      ...moreCases:
+        Array<TestCase | TestCases>
+    );
+    
+    constructor(
+      id: string,
+      cases?:
+        TestSuite
+        | TestCase
+        | TestCases,
+      ...moreCases:
+        Array<TestCase | TestCases>
+    ) {
+      this.id = id;
+      if (cases === undefined)
+        this.cases = [];
+      else
+        this.cases = this.parseInput(
+          cases,
+          ...moreCases
+        );
+    }
+    
+    run(
+      suppressLogging: boolean = false
+    ): boolean {
+      if (!suppressLogging)
+        this.cases.forEach(case => {
+          console.log(
+            case.length === 2 ?
+              case.join(", ")
+              : "ERROR: Incorrect test case syntax. "
+          )
+        });
+      return this.cases
+        .every(case => case.length === 2
+          && case[0] === case[1]
+        );
+    }
+    
+    addCase(
+      cases?:
+        TestSuite
+        | TestCase
+        | TestCases,
+      ...moreCases:
+        Array<TestCase | TestCases>
+    ): void {
+      if (cases !== undefined)
+        this.cases.push(
+          ...this.parseInput(
+            cases,
+            ...moreCases
+          )
+        );
+    }
+    
+    private parseInput(
+      cases?:
+        TestSuite
+        | TestCase
+        | TestCases,
+      ...moreCases:
+        Array<TestCase | TestCases>
+    ): TestCases {
+      return [
+        cases instanceof TestSuite ?
+          ...cases.cases
+          : ...caseOrCasesToCases(cases),
+        ...arrCaseCasesToCases(moreCases)
+      ];
+      
+      function arrCaseCasesToCases(
+        moreCases: Array<TestCase | TestCases>
+      ): TestCases {
+        const cases: TestCases = [];
+        moreCases.forEach(caseOrCases => {
+          cases.push(
+            ...toCases(caseOrCases)
+          );
+        });
+        return cases;
+      }
+      
+      function caseOrCasesToCases(
+        caseOrCases: TestCase | TestCases
+      ): TestCases {
+        return caseOrCases === [] ?
+          []
+          : Array.isArray(caseOrCases[0])?
+            [...caseOrCases]
+            : [caseOrCases];
+      }
+    }
+  }
 }
