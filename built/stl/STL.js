5da68978694e4f8398669454ca04191c07c37390
Jimmy Zhening Luo <zluo@live.com>
2023-02-10 21:21:12 +0000

    stl built

---
+++ built/stl/STL.js
@@ -0,0 +1,10 @@
+var STL;
+(function (STL) {
+    STL.shortcut = importModule("./system/Shortcut");
+    STL.rofile = importModule("./system/application/appdata/filesystem/ReadOnlyFile");
+    STL.file = importModule("./system/application/appdata/filesystem/file/File");
+    STL.url = importModule("./system/application/browser/Url");
+    STL.api = importModule("./system/application/browser/Api");
+})(STL || (STL = {}));
+module.exports = STL;
+//# sourceMappingURL=STL.js.map
