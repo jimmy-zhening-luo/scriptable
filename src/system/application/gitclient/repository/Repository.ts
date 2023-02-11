fd764a8873b013f5fa4b69af829b45b24fa5a08d
Jimmy Zhening Luo <zluo@live.com>
2023-02-11 07:07:34 +0000

    ok

--- src/system/application/gitclient/repository/Repository.ts
+++ src/system/application/gitclient/repository/Repository.ts
@@ -1,132 +1,178 @@
 abstract class Repository {
-  
+
   private readonly urlRoot: Url;
   private readonly repo: Types.stringful;
   private readonly secret: string;
-  private readonly actions: Readonly<
-    Record<
-      Repository.Action,
-      Repository.ActionHandler
-    >
-  >;
-  
+  private readonly actions: Repository.Actions;
+
   constructor(
     scheme: Types.stringful,
     host: string,
     repo: Types.stringful,
     secret: string,
-    actions?: typeof Repository.prototype.actions
+    actions: typeof Repository.prototype.actions
   ) {
-    this.urlRoot = this.instantiateRootUrl();
+    this.urlRoot = this.instantiateRootUrl(
+      scheme,
+      host
+    );
     this.repo = repo;
     this.secret = secret;
-    this.actions = actions ?? {};
+    this.actions = actions;
   }
-  
+
   private instantiateRootUrl(
     scheme: Types.stringful,
     host: string
   ): Url {
     return new Repository._Url(
-      this.scheme,
-      this.host
+      scheme,
+      host
     );
   }
-  
+
   private takeAction(
     action: Repository.Action
   ): any {
-    
-    
-    
-    function createActionUrl(
-      action: Repository.Actionq
-    ): Url {
-      
-      
-      
-      function attachIds(
-        url: Url
-      ): Url {
-        
-      }
   }
-  
+
   checkout(
     branch: Types.stringful
-  ) {
-    this
-      .createActionUrl("checkout")
-      .addParam()
-      .open();
+  ): any {
+    return this.takeAction(Repository.Action.Checkout);
   }
-  
-  pull()
-  
+
+  pull(): any {
+    return this.takeAction(Repository.Action.Pull);
+  }
+
+  commit(
+    message: Types.stringful
+  ): any {
+    return this.takeAction(Repository.Action.Commit);
+  }
+
+  push(): any {
+
+  }
+
 }
 
 namespace Repository {
-  
-  export enum Id {
-    Branch,
-    Repo,
-    Secret,
-    Message
-  }
-  
+
   export enum Action {
     Checkout,
     Pull,
     Commit,
     Push
   }
-  
-  export type Actions = {
-    checkout: ActionParams,
-    pull: ActionParams,
-    commit: ActionParams,
-    push: ActionParams,
+
+  export type ActionHandlerRecord = Readonly<
+    Record<
+      Action,
+      ActionHandler
+    >
+  >;
+
+  export interface Actions extends ActionHandlerRecord {
+    [Action.Checkout]: Checkout;
+    [Action.Pull]: Pull;
+    [Action.Commit]: Commit;
+    [Action.Push]: Push;
   }
-  
-  export class ActionHandler {
+
+  export abstract class ActionHandler {
     readonly path: string;
     readonly query: string;
-    readonly ids: IdConfig[];
-    
+    readonly params: Partial<Readonly<Record<Param, ParamConfig[]>>>;
+
     constructor(
       path: string,
       query: string,
-      ...ids: IdConfig[]
+      params: typeof ActionHandler.prototype.params
     ) {
       this.path = path;
       this.query = query;
-      this.ids = ids;
+      this.params = params;
     }
   }
-  
-  export class Id {
-    id: Id;
+
+  export class Checkout extends ActionHandler {
+    constructor(
+      path: string,
+      query: string,
+      params: {
+        [Param.Branch]: ParamConfig[]
+      }
+    ) {
+      super(path, query, params);
+    }
+  }
+
+  export class Pull extends ActionHandler {
+    constructor(
+      path: string,
+      query: string,
+      params: {}
+    ) {
+      super(path, query, params);
+    }
+  }
+
+  export class Commit extends ActionHandler {
+    constructor(
+      path: string,
+      query: string,
+      params: {
+        [Param.Message]: ParamConfig[],
+        [Param.Limit]: ParamConfig[]
+      }
+    ) {
+      super(path, query, params);
+    }
+  }
+
+  export class Push extends ActionHandler {
+    constructor(
+      path: string,
+      query: string,
+      params: {}
+    ) {
+      super(path, query, params);
+    }
+  }
+
+  export enum Param {
+    Branch,
+    Repo,
+    Secret,
+    Message,
+    Limit
+  }
+
+  export class ParamConfig {
+    param: Param;
     urlpart: UrlPart;
     prepend: string;
-    
+
     constructor(
-      id: Id,
+      param: Param,
       urlpart: UrlPart,
       prepend?: string
     ) {
-      this.id = id;
+      this.param = param;
       this.urlpart = urlpart;
       this.prepend = prepend ?? "";
     }
   }
-  
+
   export enum UrlPart {
     Path,
     Query
   }
-  
+
   export const _Url: typeof Url = importModule("./system/application/browser/Url");
-  
+
 }
 
 module.exports = Repository;
