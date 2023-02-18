c1127ae0b2c4a266c1b2e826a76202837f28ce6e
Jimmy Luo <jimmyluo@ad.corp.google.com>
2023-02-17 16:24:29 -0800

    ok

--- src/system/application/common/primitives/Types.ts
+++ src/system/application/common/primitives/Types.ts
@@ -15,46 +15,6 @@ namespace Types {
   // true primitives
   export type primitive = string | number | boolean;
 
-  // typed primitives
-  /// string
-  //// stringful
-  export type stringful = stringful.stringful<string>;
-  export namespace stringful {
-    export type stringful<S extends string> = S extends "" ? never : S;
-  }
-  export function stringful<S extends string>(string: stringful.stringful<S>) {
-    return string;
-  }
-
-  //// dotless
-  export type dotless = dotless.dotless<string>;
-  export namespace dotless {
-    export type dotless<S extends string> =
-      S extends `${string}.${string}`?
-      never
-      : S;
-  }
-  export function dotless<S extends string>(string: dotless.dotless<S>) {
-    return string;
-  }
-
-  export const dt: dotless = dotless("");
-
-  //// char
-  export type char = char.char<string>;
-  export namespace char {
-    export type char<S extends string> =
-      S extends `${string}${string}` ?
-      never
-      : S;
-  }
-  export function char<S extends string>(string: char.char<S>) {
-    return string;
-  }
-
-
-  //// charless
-
 }
 
 module.exports = Types;
