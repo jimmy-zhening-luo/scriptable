99eaaad47c42c2255fc3a1295126010eca9de8c3
Jimmy Zhening Luo <zluo@live.com>
2023-02-19 02:23:30 -0800

    apiparts built

--- src/system/application/browser/apiparts/requestparts/RequestParts.ts
+++ src/system/application/browser/apiparts/requestparts/RequestParts.ts
@@ -8,18 +8,6 @@ class RequestParts {
     return importModule("RequestHeaders");
   }
 
-  static get RequestHeaderTypes(): typeof RequestHeaderTypes {
-    return RequestParts.RequestHeaders.RequestHeaderTypes;
-  }
-
-  static get AuthRequestHeader(): typeof AuthRequestHeader {
-    return RequestParts.RequestHeaders.AuthRequestHeader;
-  }
-
-  static get GenericRequestHeader(): typeof GenericRequestHeader {
-    return RequestParts.RequestHeaders.GenericRequestHeader;
-  }
-
 }
 
 module.exports = RequestParts;
