a25c6920e454a6566e7dac92c3a077b2e7cc9815
Jimmy Zhening Luo <zluo@live.com>
2023-02-21 09:59:40 -0800

    better error logging for pack and distribution scripts

--- tools/distribute.ts
+++ tools/distribute.ts
@@ -1,93 +1,122 @@
 import { exec } from 'child_process';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
 namespace _Distribution_Tool {
 
-  export type AzCopyScriptVariables = {
-    executablePath: string;
-    sourcDistPath: string;
-    blobStoreContainerUrl: string;
-  }
+  export function distribute() {
 
-  export function distribute(azCopyScriptVariables: AzCopyScriptVariables) {
+    type AzCopyScriptVariables = {
+      executablePath: string;
+      sourcDistPath: string;
+      blobStoreContainerUrl: string;
+    }
 
     type AzCopyScripts = {
       clean: string;
       upload: string;
     }
 
-    const script_ps_exec_clean: string = [
-      `${azCopyScriptVariables.executablePath}`,
-      `rm`,
-      `"${azCopyScriptVariables.blobStoreContainerUrl}"`,
-      `--recursive;`,
-    ].join(" ");
+    console.log(`${new Date().toTimeString()}: npm run distribute: Starting distribution...`)
+    try {
+      const azCopyScriptVariables: AzCopyScriptVariables = hydrateEnv();
+      azure_clean_upload(azCopyScriptVariables);
+    } catch (e) {
+      console.error(`npm run distribute: Canceled job due to encountered error: ${e}`)
+      throw e;
+    }
 
-    const script_ps_exec_upload: string = [
-      `${azCopyScriptVariables.executablePath}`,
-      `copy`,
-      `"${azCopyScriptVariables.sourcDistPath}"`,
-      `"${azCopyScriptVariables.blobStoreContainerUrl}"`,
-      "--recursive;",
-    ].join(" ");
+    function hydrateEnv(): AzCopyScriptVariables {
+      try {
+        dotenv.config();
+        const TEMPLATE_EXAMPLE_URL_BLOBSTORE_CONTAINER: string = "https://<your-blob-store>.blob.core.windows.net/<your-container>";
+        const azCopyScriptVariables: AzCopyScriptVariables = {
+          executablePath: process.env.PATH_AZCOPY_EXECUTABLE
+            || "C:\\azcopy\\azcopy.exe",
+          sourcDistPath: "dist",
+          blobStoreContainerUrl: process.env.URL_AZURE_BLOB_STORE_CONTAINER
+            || TEMPLATE_EXAMPLE_URL_BLOBSTORE_CONTAINER,
+        };
+        if (azCopyScriptVariables.blobStoreContainerUrl === TEMPLATE_EXAMPLE_URL_BLOBSTORE_CONTAINER)
+          throw new ReferenceError(`npm run distribute: hydrateEnv: Canceled distribution because no environment variable was set for URL_AZURE_BLOB_STORE_CONTAINER. Check your deployment environment variables or your .env file. See example URL_AZURE_BLOB_STORE_CONTAINER: ${TEMPLATE_EXAMPLE_URL_BLOBSTORE_CONTAINER}`);
+        return azCopyScriptVariables;
+      } catch (e) {
+        console.error(`npm run distribute: hydrateEnv: Error while loading environmental variables to specify where to distribute the packed files: ${e}`);
+        throw e;
+      }
+    }
 
-    ps_exec({
-      clean: script_ps_exec_clean,
-      upload: script_ps_exec_upload,
-    });
+    function azure_clean_upload(azCopyScriptVariables: AzCopyScriptVariables) {
+      const ps_exec_scripts: AzCopyScripts = {
+        clean: [
+          `${azCopyScriptVariables.executablePath}`,
+          `rm`,
+          `"${azCopyScriptVariables.blobStoreContainerUrl}"`,
+          `--recursive;`,
+        ].join(" "),
+        upload: [
+          `${azCopyScriptVariables.executablePath}`,
+          `copy`,
+          `"${azCopyScriptVariables.sourcDistPath}"`,
+          `"${azCopyScriptVariables.blobStoreContainerUrl}"`,
+          "--recursive;",
+        ].join(" ")
+      };
 
-    function ps_exec(azCopyScripts: AzCopyScripts) {
-      clean(azCopyScripts);
-      function clean(azCopyScripts: AzCopyScripts) {
-        exec(azCopyScripts.clean, { 'shell': 'powershell.exe' }, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          if (stderr) {
-            console.error(`stderr: ${stderr}`);
-          }
-          console.log(`stdout: ${stdout}`);
-          upload(azCopyScripts);
-        });
+      try {
+        ps_exec(
+          ps_exec_scripts.clean,
+          ps_exec_scripts.upload
+        )
+      } catch (e) {
+        console.error(`npm run distribute: azure_clean_upload: Error while executing azcopy commands: ${e}`);
+        throw e;
+      }
 
-        function upload(azCopyScripts: AzCopyScripts) {
-          exec(azCopyScripts.upload, { 'shell': 'powershell.exe' }, (error, stdout, stderr) => {
+      function ps_exec(azCopyScript: string, ...nextScript: string[]) {
+        try {
+          exec(azCopyScript, { 'shell': 'powershell.exe' }, (error, stdout, stderr) => {
             if (error) {
-              console.error(`exec error: ${error}`);
-              return;
+              throw error;
             }
             if (stderr) {
-              console.error(`stderr: ${stderr}`);
+              throw new Error(stderr);
             }
-            console.log(`stdout: ${stdout}`);
+            if (azCopySucceeded(stdout)) {
+              console.log(`npm run distribute: azure_clean_upload: ps_exec: SUCCESS, Azure Storage Web API returned success.`)
+              if (nextScript !== undefined && nextScript.length > 0)
+                ps_exec(nextScript[0], ...nextScript.slice(1));
+              else
+                console.log(`${new Date().toTimeString()}: npm run distribute: Job completed successfully without errors.`);
+            }
+            else {
+              console.log(`npm run distribute: azure_clean_upload: ps_exec: Completed with errors (partial success): ${stdout}`);
+              if (nextScript !== undefined && nextScript.length > 0)
+                ps_exec(nextScript[0], ...nextScript.slice(1));
+              else
+                console.log(`${new Date().toTimeString()}: npm run distribute: Job completed successfully without errors.`);
+
+            }
           });
+        } catch (e) {
+          console.error(`npm run distribute: azure_clean_upload:ps_exec: Error while executing powershell: ${e}`);
+          throw e;
         }
+      }
+
+      function azCopySucceeded(
+        stdout: string,
+      ): boolean {
+        const successCriteriaOutputLines: string[] = [
+          `Number of File Transfers Failed: 0`,
+          `Number of Folder Transfers Skipped: 0`,
+          `Number of File Transfers Skipped: 0`,
+          `Number of Folder Transfers Skipped: 0`,
+          `Final Job Status: Completed`
+        ]
+        return successCriteriaOutputLines.every(successOutputLIne => stdout.includes(successOutputLIne));
       }
     }
   }
 }
 
-const blobStoreContainerUrlTemplate: string = "https://<your-blob-store>.blob.core.windows.net/<your-container>";
-
-const azCopyScriptVariables: _Distribution_Tool.AzCopyScriptVariables = {
-
-  executablePath:
-    process.env.PATH_AZCOPY_EXECUTABLE
-    || "C:\\azcopy\\azcopy.exe",
-
-  sourcDistPath:
-    "dist",
-
-  blobStoreContainerUrl:
-    process.env.URL_AZURE_BLOB_STORE_CONTAINER
-    || blobStoreContainerUrlTemplate,
-
-}
-
-if (azCopyScriptVariables.blobStoreContainerUrl === blobStoreContainerUrlTemplate)
-  console.warn("npm run distribute: Canceled distribution because no environment variable was set for URL_AZURE_BLOB_STORE_CONTAINER. Check your deployment environment variables or your .env file.");
-else
-  _Distribution_Tool.distribute(azCopyScriptVariables);
+_Distribution_Tool.distribute();
--- tools/pack.ts
+++ tools/pack.ts
@@ -1,32 +1,71 @@
 import * as shell from "shelljs";
+import rimraf from "rimraf";
 
-shell.mkdir("dist");
+namespace _Pack_Tool {
 
-let builtFileCounter: number = 0;
-shell.ls("built").forEach((file) => {
-  builtFileCounter++;
-  shell.cp("-R", `built/${file}`, "dist");
-});
+  export function pack() {
+    console.log(`${new Date().toTimeString()}: npm run pack: Starting pack job...`);
 
-if (builtFileCounter === 0)
-  console.log("npm run pack: No built files found in './built'");
+    try {
+      rimraf.sync("dist");
+    }
+    catch (e) {
+      console.error(`npm run pack: Error deleting './dist' folder using npm package 'rimraf': ${e}`);
+      throw e;
+    }
 
-let distFileCounter: number = 0;
-shell.ls("-R", "dist").forEach((file) => {
-  distFileCounter++;
-  if (
-    file.endsWith(".d.ts")
-    || file.endsWith(".d.ts.map")
-    || file.endsWith(".js.map")
-    || file.endsWith("tsconfig.tsbuildinfo")
-  ) {
-    shell.rm("-rf", `dist/${file}`);
+    try {
+      try {
+        shell.mkdir("dist");
+        if (shell.ls(".").indexOf("dist") === -1)
+          throw new ReferenceError(`npm run pack: After executing function to create a './dist' folder, a dist folder should exist. However, could not find './dist' folder.`);
+      } catch (e) {
+        console.error(`npm run pack: Error creating new './dist' folder: ${e}`);
+        throw e;
+      }
+
+      let builtFileCounter: number = 0;
+      try {
+        shell.ls("built").forEach((file) => {
+          builtFileCounter++;
+          shell.cp("-R", `built/${file}`, "dist");
+        });
+        if (builtFileCounter === 0)
+          throw new ReferenceError(`npm run pack: Pack should execute after Build to move built files to dist. However, no built files were found in './built. Are you sure that you have executed 'npm run build' before 'npm run pack'?`);
+      } catch (e) {
+        console.error(`npm run pack: Error copying all built files to './dist': ${e}`);
+        throw e;
+      }
+
+      let distFileCounter: number = 0;
+      try {
+        shell.ls("-R", "dist").forEach((file) => {
+          distFileCounter++;
+          if (
+            file.endsWith(".d.ts")
+            || file.endsWith(".d.ts.map")
+            || file.endsWith(".js.map")
+            || file.endsWith("tsconfig.tsbuildinfo")
+          ) {
+            shell.rm("-rf", `dist/${file}`);
+          }
+        });
+        if (
+          builtFileCounter !== 0
+          && distFileCounter === 0
+        )
+          throw new ReferenceError(`npm run pack: After copying n=${builtFileCounter} top-level files or directories from './built' to './dist', there should be exactly n=${builtFileCounter} top-level items in './dist'. However, 0 top-level items were found in './dist'`);
+      } catch (e) {
+        console.error(`npm run pack: Error filtering copied files in './dist': ${e}`);
+        throw e;
+      }
+
+      console.log(`${new Date().toTimeString()}: npm run pack: Pack job completed successfully`)
+    } catch (e) {
+      console.error(`npm run pack: Error executing pack job: ${e}`);
+      throw e;
+    }
   }
-});
+}
 
-if (
-  builtFileCounter !== 0
-  && distFileCounter === 0
-)
-  console.log(`npm run pack: After copying ${builtFileCounter} built files to dist, no dist files were found in './dist'`);
-  
+_Pack_Tool.pack();
