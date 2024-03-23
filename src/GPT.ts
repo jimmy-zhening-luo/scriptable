ffd6327d7efa04f4c26490e81569592793337636
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-03-23 14:16:33 -0700

    GPT: migrate input handling to shortcutParameter

--- src/GPT.ts
+++ src/GPT.ts
@@ -11,74 +11,71 @@ namespace GPT {
   export class GPT extends shortcut {
     public runtime(): GPTOutput {
       try {
-        // Get settings
-        const {
-          app,
-          user,
-        }: GPTSettings = this
-          .setting
-          .unmerged as GPTSettings;
-
-        // Get Shortcut raw input (string)
-        const rawInput: string = this
+        // Get Shortcut input
+        const input: undefined | string | GPTInput = this
           .input
-          .plainTexts[0] ?? "";
+          .shortcutParameter as undefined | string | GPTInput;
 
-        // Parse input (string) into GPTInput
-        const input: GPTInput = !rawInput.startsWith("{")
-          ? {
-              prompt: rawInput,
-            }
-          : JSON.parse(rawInput) as GPTInput;
+        // Validate input is a Dictionary
+        if (input === undefined || typeof input === "string")
+          throw new TypeError(
+            `Shortcut input must be dictionary. Instead it was undefined or string: ${String(input as string)}`
+          );
+        else {
+          // Get settings
+          const {
+            app,
+            user,
+          }: GPTSettings = this
+            .setting
+            .unmerged as GPTSettings;
 
-        if (input.prompt.endsWith("=") && !input.prompt.includes(" "))
-          input.prompt = atob(input.prompt);
-
-        // Fill in blank options with defaults
-        const final: GPTFinal = {
-          prompt: input.prompt,
-          model: input.model ?? user.default.model,
-          token:
-            input.token !== undefined
-            && Number.isInteger(input.token)
-            && input.token <= app.limit.token
-              ? input.token
-              : user.default.token,
-          temperature:
-            input.temperature !== undefined
-            && Number.isFinite(input.temperature)
-            && input.temperature >= app.limit.temperature.min
-            && input.temperature <= app.limit.temperature.max
-              ? input.temperature
-              : user.default.temperature,
-          preset: input.preset !== undefined && input.preset in user.presets
-            ? input.preset
-            : user.default.preset,
-        };
-
-        // Build user message
-        const preset: undefined | PresetPrompt = user.presets[final.preset];
-
-        const message: GPTOutput["message"] = !preset
-          ? {
-              user: final.prompt,
-            }
-          : {
-              system: preset.system,
-              user: preset.user === undefined
-                ? final.prompt
-                : !preset.user.includes(app.presetTag)
-                    ? final.prompt
-                    : preset.user.replace(app.presetTag, final.prompt),
-            };
-
-        // Build GPTResponse from ChatOptions & return GPTResponse to Shortcut
-        return {
-          message,
-          model: app.models[final.model],
-          token: final.token,
-          temperature: final.temperature,
-        };
+          // Fill in blank options with defaults
+          const final: GPTFinal = {
+            prompt: input.prompt,
+            model: input.model ?? user.default.model,
+            token:
+              input.token !== undefined
+              && Number.isInteger(input.token)
+              && input.token <= app.limit.token
+                ? input.token
+                : user.default.token,
+            temperature:
+              input.temperature !== undefined
+              && Number.isFinite(input.temperature)
+              && input.temperature >= app.limit.temperature.min
+              && input.temperature <= app.limit.temperature.max
+                ? input.temperature
+                : user.default.temperature,
+            preset: input.preset !== undefined && input.preset in user.presets
+              ? input.preset
+              : user.default.preset,
+          };
+  
+          // Build user message
+          const preset: undefined | PresetPrompt = user.presets[final.preset];
+  
+          const message: GPTOutput["message"] = !preset
+            ? {
+                user: final.prompt,
+              }
+            : {
+                system: preset.system,
+                user: preset.user === undefined
+                  ? final.prompt
+                  : !preset.user.includes(app.presetTag)
+                      ? final.prompt
+                      : preset.user.replace(app.presetTag, final.prompt),
+              };
+  
+          // Build GPTResponse from ChatOptions & return GPTResponse to Shortcut
+          return {
+            message,
+            model: app.models[final.model],
+            token: final.token,
+            temperature: final.temperature,
+          };
+        }
       }
       catch (e) {
         throw new EvalError(`GPT: runtime: Error running app: \n${e as string}`);
