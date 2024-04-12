c41e8d4d2a2eeea697fd958a58733f3ac6a538f4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-04-12 15:51:18 -0700

    search: still missing the empty input / full clipboard case

--- src/Search.ts
+++ src/Search.ts
@@ -30,7 +30,9 @@ namespace Search {
           const MATH_KEYS: string | string[] = app.mathKeys ?? [];
 
           const query: Query = new Query(
-            this.inputData?.input ?? this.readStorage(),
+            this.inputData?.input === ""
+              ? this.readStorage()
+              : this.inputData?.input,
             this.inputData?.clip ?? "",
             MATH_KEYS,
           );
