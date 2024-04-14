3f07a2b0fd976f88c63332cf488829306aa13d07
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-04-13 20:42:41 -0700

    common/types: Delete Numbers / Tuples

--- src/common/types/Types.ts
+++ src/common/types/Types.ts
@@ -8,15 +8,6 @@ class Types {
     }
   }
 
-  public static get Numbers(): typeof Numbers {
-    try {
-      return importModule("numbers/Numbers") as typeof Numbers;
-    }
-    catch (e) {
-      throw new ReferenceError("Types: error importing Numbers module");
-    }
-  }
-
   public static get Strings(): typeof Strings {
     try {
       return importModule("strings/Strings") as typeof Strings;
