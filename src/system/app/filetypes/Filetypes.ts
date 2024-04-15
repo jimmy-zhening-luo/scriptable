9c21ead7b19005fb6e5ad15b7c080b66d996bd00
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-04-14 23:12:55 -0700

    Filetypes: remove Secret; hide Filetype/Files

--- src/system/app/filetypes/Filetypes.ts
+++ src/system/app/filetypes/Filetypes.ts
@@ -1,26 +1,24 @@
 class Filetypes {
-  public static get Filetype(): typeof Filetype {
-    return Filetypes.Setting.Filetype;
-  }
-
-  public static get IOFile(): typeof IOFile {
-    return Filetypes.Filetype.IOFile;
-  }
-
-  public static get ReadOnlyIOFile(): typeof ReadOnlyIOFile {
-    return Filetypes.Filetype.ReadOnlyIOFile;
-  }
-
   public static get Setting(): typeof Setting {
-    return importModule("Setting") as typeof Setting;
-  }
-
-  public static get Secret(): typeof Secret {
-    return importModule("Secret") as typeof Secret;
+    try {
+      return importModule("Setting") as typeof Setting;
+    }
+    catch (e) {
+      throw new ReferenceError(
+        `Filetypes: import Setting: \n${e as string}`,
+      );
+    }
   }
 
   public static get Storage(): typeof Storage {
-    return importModule("Storage") as typeof Storage;
+    try {
+      return importModule("Storage") as typeof Storage;
+    }
+    catch (e) {
+      throw new ReferenceError(
+        `Filetypes: import Storage: \n${e as string}`,
+      );
+    }
   }
 }
 
