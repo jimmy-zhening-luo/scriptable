5fdfe138a59d8fff02ceca9f5eeaf13b2b532913
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-07 23:20:14 -0700

    +type/literal: errorlike

---
+++ src/common/types/literals/errorlike.d.ts
@@ -0,0 +1,6 @@
+declare const error: unique symbol;
+declare type ErrorLike<E extends boolean = false> = { [error]: "ErrorLike" } & E extends true
+  ? Error
+  : string | Error | Partial<
+      Record<"message" | "cause", ErrorLike>
+    >;
