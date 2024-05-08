7c33766d6d3bac1a28654061794b856ddadd150f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-07 23:26:09 -0700

    errorlike: no recursion needed, just need to infer: error | obj with cause (fake error) | string-castable

--- src/common/types/literals/errorlike.d.ts
+++ src/common/types/literals/errorlike.d.ts
@@ -1,6 +1,9 @@
 declare const error: unique symbol;
 declare type ErrorLike<E extends boolean = false> = { [error]: "ErrorLike" } & E extends true
   ? Error
-  : string | Error | Partial<
-      Record<"message" | "cause", ErrorLike>
-    >;
+  : string[] | Error | Partial<
+    Record<
+      "cause",
+      Error
+    >
+  >;
