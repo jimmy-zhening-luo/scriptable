9323a53a1ad1edeacf9c57532f619fe59b222519
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-13 15:35:11 -0700

    note to self: do not hand lint in the future

--- src/system/app/browsers/browser/url/Url.ts
+++ src/system/app/browsers/browser/url/Url.ts
@@ -65,18 +65,6 @@ class Url {
     }
   }
 
-  public get url(): stringful {
-    try { // TBD
-      return this.scheme;
-    }
-    catch (e) {
-      throw new EvalError(
-        `Url: url`,
-        { cause: e },
-      );
-    }
-  }
-
   protected static get stringful(): typeof Stringful {
     try {
       return importModule(
@@ -100,6 +88,18 @@ class Url {
     catch (e) {
       throw new ReferenceError(
         `Url: import PosInt`,
+        { cause: e },
+      );
+    }
+  }
+
+  public get url(): stringful {
+    try { // TBD
+      return this.scheme;
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: url`,
         { cause: e },
       );
     }
