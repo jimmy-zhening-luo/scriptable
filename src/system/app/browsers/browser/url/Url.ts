f1a70e57390f5592a6e5948c4a04bc7e61b56f80
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-13 14:42:52 -0700

    WIP: Url: class with stubbed to:'url', getters/setters for 'path' | 'query' | 'fragment'

---
+++ src/system/app/browsers/browser/url/Url.ts
@@ -0,0 +1,244 @@
+class Url {
+  public readonly scheme: stringful;
+  public readonly host: string;
+  public readonly port: Nullable<posint>;
+  private _path: stringful[] = [];
+  private _query: Record<stringful, string> = {};
+  private _fragment: string = "";
+
+  constructor(
+    base: string | Url,
+    host?: string,
+    port?: Nullable<number>,
+    path?: string | string[],
+    query?: string | Record<string, string>,
+    fragment?: string,
+  ) {
+    try {
+      { this } = typeof Url === "string"
+        ? Url.parse(base)
+        : Url;
+
+      if (typeof host !== "undefined")
+        this.host = host;
+      if (typeof port !== "undefined")
+        this.port = port === null
+          ? null
+          : Url.posint(
+              port,
+            );
+      if (typeof path !== "undefined")
+        this.path = path;
+      if (typeof query !== "undefined")
+        this.query = query;
+      if (typeof fragment !== "undefined")
+        this.fragment = fragment;
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: ctor`,
+        { cause: e },
+      );
+    }
+  }
+
+  protected static get stringful(): typeof Stringful {
+    try {
+      return importModule(
+        "./common/types/literals/string/Stringful",
+      ) as typeof Stringful;
+    }
+    catch (e) {
+      throw new ReferenceError(
+        `Url: import Stringful`,
+        { cause: e },
+      );
+    }
+  }
+
+  protected static get posint(): typeof PosInt {
+    try {
+      return importModule(
+        "./common/types/literals/number/PosInt",
+      ) as typeof PosInt;
+    }
+    catch (e) {
+      throw new ReferenceError(
+        `Url: import PosInt`,
+        { cause: e },
+      );
+    }
+  }
+
+  private static parse(url: string): ParsedUrl {
+    try { // TBD
+      return {
+        scheme: "foo" as stringful,
+        host: url,
+        port: null,
+      }
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: parse`,
+        { cause: e },
+      );
+    }
+  }
+
+  public get url(): stringful {
+    try { // TBD
+      return this.scheme;
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: url`,
+        { cause: e },
+      );
+    }
+  }
+
+  public get path(): string {
+    try {
+      return this.path
+        .join(
+          "/",
+        );
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: path`,
+        { cause: e },
+      );
+    }
+  }
+
+  public get query(): string {
+    try {
+      return Object
+        .entries(
+          this._query,
+        )
+        .map(
+          (entry: [string, string]): string =>
+            entry
+              .join(
+                "=",
+              ),
+        )
+        .join(
+          "&",
+        );
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: query`,
+        { cause: e },
+      );
+    }
+  }
+
+  public get fragment(): string {
+    try {
+      return this._fragment;
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: fragment`,
+        { cause: e },
+      );
+    }
+  }
+
+  public set path(
+    path: string | string[],
+  ) {
+    try { // TBD
+      if (typeof path === "string")
+        this._path = [];
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: set path`,
+        { cause: e },
+      );
+    }
+  }
+
+  public set query(
+    query: string | Record<string, string>,
+  ) {
+    try { // TBD
+      if (typeof query === "string")
+        this._query = {};
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: set query`,
+        { cause: e },
+      );
+    }
+  }
+
+  public set fragment(
+    fragment: string,
+  ) {
+    try { // TBD
+      this._fragment = fragment;
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: set fragment`,
+        { cause: e },
+      );
+    }
+  }
+
+  public append(
+    subpath: string | string[],
+  ): this {
+    try { // TBD
+      if (typeof subpath === "string")
+        this._path = [];
+
+      return this;
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: append`,
+        { cause: e },
+      );
+    }
+  }
+
+  public addQuery(
+    params: string | Record<string, string>,
+  ): this {
+    try { // TBD
+      if (typeof params === "string")
+        this._query = {};
+
+      return this;
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: addQuery`,
+        { cause: e },
+      );
+    }
+  }
+
+  public toString(): stringful {
+    try {
+      return this.url;
+    }
+    catch (e) {
+      throw new EvalError(
+        `Url: toString`,
+        { cause: e },
+      );
+    }
+  }
+}
+
+module.exports = Url;
