187f1017059ddc2e6f53b30b6623f70e8da49e1b
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-14 16:37:47 -0700

    merge GPTInput + Options

--- src/apps/input/GPTInput.d.ts
+++ src/apps/input/GPTInput.d.ts
@@ -7,3 +7,18 @@ declare type GPTPrompt = {
   system: string;
   user: string;
 };
+
+declare type GPTInputOptions = {
+  prompt:
+    | string
+    | GPTPrompt;
+  token?: number;
+  temperature?: number;
+  p?: number;
+  model?:
+    | "ultra"
+    | "high"
+    | "low";
+  preset?: string;
+  location?: string;
+};
