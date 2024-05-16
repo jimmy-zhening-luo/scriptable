fac253e191c3cc8928a31b1e9d8778bbdd757834
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-15 18:48:34 -0700

    Add GptMessages type declaration

---
+++ src/apps/types/gpt/GptMessages.d.ts
@@ -0,0 +1,11 @@
+declare type GptMessages<S extends boolean = false> = S extends false
+  ? [GptMessage<"user">]
+  : [GptMessage<"system">, GptMessage<"user">];
+
+declare type GptMessage<R extends
+  | "system"
+  | "user"
+> = {
+  role: R;
+  content: string;
+};
