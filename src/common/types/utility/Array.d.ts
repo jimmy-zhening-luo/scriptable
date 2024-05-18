b4622aa9eb23306d0fd6cc2024eb7a5a6dee2169
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-18 16:05:13 -0700

    Array: s/T/I for inner/iterable/item, plug in Indexable util

--- src/common/types/utility/Array.d.ts
+++ src/common/types/utility/Array.d.ts
@@ -1,35 +1,32 @@
-declare type Oneple<T> = Tuple<T, 1>;
-declare type Twople<T> = Tuple<T, 2>;
-declare type Threeple<T> = Tuple<T, 3>;
-declare type Fourple<T> = Tuple<T, 4>;
-declare type Fiveple<T> = Tuple<T, 5>;
-declare type Sixple<T> = Tuple<T, 6>;
-declare type Sevenple<T> = Tuple<T, 7>;
-declare type Eightple<T> = Tuple<T, 8>;
-declare type Nineple<T> = Tuple<T, 9>;
-declare type Tenple<T> = Tuple<T, 10>;
+declare type Oneple<I> = Tuple<I, 1>;
+declare type Twople<I> = Tuple<I, 2>;
+declare type Threeple<I> = Tuple<I, 3>;
+declare type Fourple<I> = Tuple<I, 4>;
+declare type Fiveple<I> = Tuple<I, 5>;
+declare type Sixple<I> = Tuple<I, 6>;
+declare type Sevenple<I> = Tuple<I, 7>;
+declare type Eightple<I> = Tuple<I, 8>;
+declare type Nineple<I> = Tuple<I, 9>;
+declare type Tenple<I> = Tuple<I, 10>;
 
-declare type Arrayful<T> = ArrayMin<T, 1>;
-declare type Array2<T> = ArrayMin<T, 2>;
-declare type Array3<T> = ArrayMin<T, 3>;
-declare type Array4<T> = ArrayMin<T, 4>;
-declare type Array5<T> = ArrayMin<T, 5>;
+declare type Arrayful<I> = ArrayMin<I, 1>;
+declare type Array2<I> = ArrayMin<I, 2>;
+declare type Array3<I> = ArrayMin<I, 3>;
+declare type Array4<I> = ArrayMin<I, 4>;
+declare type Array5<I> = ArrayMin<I, 5>;
 
-declare type Tuple<T, L extends number> = T[] & {
-  0: T;
-  length: L;
-};
+declare type Tuple<I, L extends number> = I[] & Indexable<I, L>;
 
-declare type ArrayMin<T, L extends number> = L extends L
+declare type ArrayMin<I, L extends number> = L extends L
   ? number extends L
     ? never
-    : BuildArray<T, L, []>
+    : BuildArray<I, L, []>
   : never;
 
 type BuildArray<
-  T,
+  I,
   L extends number,
-  Head extends T[],
+  Head extends I[],
 > = Head["length"] extends L
-  ? [...Head, ...T[]]
-  : BuildArray<T, L, [...Head, T]>;
+  ? [...Head, ...I[]]
+  : BuildArray<I, L, [...Head, I]>;
