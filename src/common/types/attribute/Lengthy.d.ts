94158ab13cd1ae52719c281d7cb376e3c1e736d4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-18 15:58:57 -0700

    Stringlength

--- src/common/types/attribute/Lengthy.d.ts
+++ src/common/types/attribute/Lengthy.d.ts
@@ -4,18 +4,22 @@ type OldLength<L> = { [length]: L };
 
 declare type Lengthy<L extends number, T> = T & OldLength<L>;
 
-type Length<L> = { length: L };type Head<I> = { 0: I };type Iterable<
-  I,
-  L extends number,
-> = Length<L> & Head<I>;
+type Length<L extends number> = { length: L };
+type Head<I> = { 0: I };
 
-declare type NewLengthy<
-  T extends iterable,
+declare type Indexable<
   L extends number,
+  I,
 > = L extends 0
   ? never
-  : T & Iterable<
-    T extends Array<infer I> ? I : T,
-    L
-  >
+  : Length<L> & Head<I>
+;
+
+declare type StringLength<
+  L extends number,
+  S extends stringful,
+  S0 extends stringful = S,
+> =
+  & S
+  & Indexable<L, S0>
 ;
--- src/common/types/predicate/iterable.d.ts
+++
@@ -1,4 +0,0 @@
-declare type iterable =
-  | string
-  | unknown[]
-;
