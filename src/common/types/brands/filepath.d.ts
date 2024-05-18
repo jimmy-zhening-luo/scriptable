c952e358cd625941cfd1ae9366bde79cd1e6b592
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-17 22:13:51 -0700

    linted

--- src/common/types/brands/filepath.d.ts
+++ src/common/types/brands/filepath.d.ts
@@ -1,6 +1,6 @@
 declare const fileroot: unique symbol;
 
-type FStringBrand<B> = { [fileroot]: B }
+type FStringBrand<B> = { [fileroot]: B };
 
 declare type filestring<Root extends boolean = false> = FStringBrand<Root> & Root extends true
   ? stringful
