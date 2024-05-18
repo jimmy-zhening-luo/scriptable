1d96dd1818e87830f3a469be37a115f9eeeb6913
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-17 22:47:03 -0700

    filepath brand: clarify brand meaning

--- src/common/types/brands/filepath.d.ts
+++ src/common/types/brands/filepath.d.ts
@@ -1,8 +1,8 @@
 declare const fileroot: unique symbol;
 
-type FStringBrand<B> = { [fileroot]: B };
+type FilepathLevel<B> = { [fileroot]: B };
 
-declare type filepath<Root extends boolean = false> = FStringBrand<Root> & Root extends true
+declare type filepath<Root extends boolean = false> = FilepathLevel<Root> & Root extends true
   ? stringful
   : string;
 
