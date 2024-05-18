bbe91dce5f32a13057eb5a60d0642f2b209ac08a
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-17 22:54:24 -0700

    brands: rm unnecessary inner type constraints

--- src/common/types/brands/Lengthy.d.ts
+++ src/common/types/brands/Lengthy.d.ts
@@ -1,5 +1,5 @@
 declare const length: unique symbol;
 
-type Length<L extends number> = { [length]: L };
+type Length<L> = { [length]: L };
 
 declare type Lengthy<L extends number, T> = T & Length<L>;
--- src/common/types/brands/Validated.d.ts
+++ src/common/types/brands/Validated.d.ts
@@ -1,5 +1,5 @@
 declare const validator: unique symbol;
 
-type ValidatedBy<V extends string> = { [validator]: V };
+type ValidatedBy<V> = { [validator]: V };
 
 declare type Validated<V extends string, T> = T & ValidatedBy<V>;
