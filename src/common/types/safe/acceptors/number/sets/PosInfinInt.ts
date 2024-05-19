252da3de89627a30ac7d1f8a23525fe486607fee
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-18 22:32:17 -0700

    typo2

--- src/common/types/safe/acceptors/number/sets/PosInfinInt.ts
+++ src/common/types/safe/acceptors/number/sets/PosInfinInt.ts
@@ -5,9 +5,9 @@ const piint_Primitiveful: typeof Primitiveful = importModule(
 function PosInfinInt(
   number: number,
   context?: string,
-): posint {
+): posinfinint {
   return piint_Primitiveful<number, "posinfinint", posinfinint>(
-    (number: number): number is posint =>
+    (number: number): number is posinfinint =>
       Number.isInteger(number) && number > 0 || number === Infinity,
     number,
     `posinfinint: number is not an [infinite] integer`,
