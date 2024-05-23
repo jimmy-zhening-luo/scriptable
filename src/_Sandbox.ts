2d7726bdfc8946afa78122309df3678c2579e14b
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-22 19:40:09 -0700

    +sandbox

---
+++ src/_Sandbox.ts
@@ -0,0 +1,8 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-green; icon-glyph: leaf;
+"use strict";
+
+const foo = true;
+
+console.log(foo);
