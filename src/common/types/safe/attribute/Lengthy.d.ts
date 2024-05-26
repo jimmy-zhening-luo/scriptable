ad2935f8a8db545a72258703b0deca505f9742bb
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-25 22:29:07 -0700

    WIP: undo lengthy: rm safeiterable

--- src/common/types/safe/attribute/Lengthy.d.ts
+++ src/common/types/safe/attribute/Lengthy.d.ts
@@ -17,30 +17,3 @@ declare type Length<
 declare type Head<
   I,
 > = { 0: I };
-
-declare type SafeIterable<
-  L extends number,
-  I,
-> = L extends 0
-  ? never
-  :
-    & Length<
-      L
-    >
-    & Head<
-      I
-    >;
-
-declare type StringLength<
-  L extends number,
-  S extends stringful,
-  S0 extends stringful = S,
-> =
-  & S
-  & SafeIterable<
-    L,
-    & S0
-    & Length<
-      1
-    >
-  >;
