cd32e9bc87ac17affc3145bc8cfdfc87d60828d6
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-31 20:51:47 -0700

    upgrade lint to v9; remove tslint deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,12 @@
 import stylistic from "@stylistic/eslint-plugin";
-import ts from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
 import Lint from "@jimbojet/lint";
 
 export default [
   ...new Lint(
+    stylistic,
     {
-      stylistic,
-      files: ["*.config.js"],
-    },
-    {
-      ts,
-      parser,
-      files: ["src/**/*.ts"],
+      js: ["*.config.js"],
+      ts: ["src/**/*.ts"],
     },
     { overrideTs: { "@typescript-eslint/explicit-module-boundary-types": "off" } },
   )
