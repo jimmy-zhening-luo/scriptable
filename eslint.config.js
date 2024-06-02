9b322d58ec017be0e03a901288b77ec10e4d03c8
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-01 22:03:57 -0700

    upgrade lint v10 (remove dep on stylistic)

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
-import stylistic from "@stylistic/eslint-plugin";
 import Lint from "@jimbojet/lint";
 
 export default [
   ...new Lint(
-    stylistic,
     {
       js: ["*.config.js"],
       ts: ["src/**/*.ts"],
