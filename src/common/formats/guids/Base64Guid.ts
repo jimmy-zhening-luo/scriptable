a26ba1111b926b01a520528dcc3378f64782a5f4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-02 11:53:15 -0700

    replace base64 lookup table with arithmetic to save size

--- src/common/formats/guids/Base64Guid.ts
+++ src/common/formats/guids/Base64Guid.ts
@@ -30,7 +30,7 @@ function Base64Guid() {
       encodedGuid
         .length === 8
     )
-      return encodedGuid as stringful; // unsafe
+      return encodedGuid as Guid<"base64">; // unsafe
     else
       throw new TypeError(
         `Unexpected: invalid base64-encoded guid (length !== 8)`,
@@ -61,72 +61,6 @@ function Base64Guid() {
     E: 14,
     F: 15,
   } as const;
-  const base64ToBase64Char = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "+",
-    "/",
-  ] as const;
 
   try {
     const buffer: Octad<
@@ -147,7 +81,8 @@ function Base64Guid() {
         .replaceAll(
           "-",
           "",
-        ),
+        )
+      ,
     );
 
     for (
@@ -169,52 +104,62 @@ function Base64Guid() {
           ],
         );
 
-    const encodedGuid = base64guid(
-      buffer
-        .filter(
-          (row): row is Quad<
-            hex
-          > =>
-            row
-              .length === 4,
-        )
-        .map(
-          ([
-            q0,
-            q1,
-            q2,
-            q3,
-          ]) =>
-            (
-              q0
-              + q1
-              + q2
-              + q3
-            ) as base64,
-        )
-        .filter(
-          (word): word is base64 =>
-            word >= 0
-            && word < 64,
-        )
-        .map(
-          word =>
-            base64ToBase64Char[
-              word
-            ],
-        )
-        .filter(
-          (base64char): base64char is base64char =>
-            typeof base64char === "string",
-        )
-        .join(
-          "",
+    const chars = buffer
+      .filter(
+        (row): row is Quad<
+          hex
+        > =>
+          row
+            .length === 4,
+      )
+      .map(
+        ([
+          q0,
+          q1,
+          q2,
+          q3,
+        ]) =>
+          (
+            q0
+            + q1
+            + q2
+            + q3
+          ) as base64,
+      )
+      .filter(
+        (word): word is base64 =>
+          word >= 0
+          && word < 64,
+      )
+      .map(
+        word =>
+          word + 43,
+      )
+      .map(
+        charRangePlus =>
+          charRangePlus > 43
+            ? charRangePlus + 3
+            : charRangePlus,
+      )
+      .map(
+        charRangeSlashDigit =>
+          charRangeSlashDigit > 57
+            ? charRangeSlashDigit + 7
+            : charRangeSlashDigit,
+      )
+      .map(
+        charRangeAlphaUpper =>
+          charRangeAlphaUpper > 90
+            ? charRangeAlphaUpper + 6
+            : charRangeAlphaUpper,
+      );
+
+    return base64guid(
+      String
+        .fromCharCode(
+          ...chars,
         ),
     );
-
-    return encodedGuid as Guid<
-      "base64"
-    >;
   }
   catch (e) {
     throw new EvalError(
