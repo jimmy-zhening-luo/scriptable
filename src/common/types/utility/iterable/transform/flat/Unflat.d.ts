4b10ec4660a17e45e8477e92efa22051a501daa7
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-19 22:57:36 -0700

    immutable unflat by default

--- src/common/types/utility/iterable/transform/flat/Unflat.d.ts
+++ src/common/types/utility/iterable/transform/flat/Unflat.d.ts
@@ -1,14 +1,22 @@
 declare type Unflat<
   Inner,
+  Mutable extends boolean = false,
 > =
   | Inner
-  | Inner[]
+  | (
+    Mutable extends true
+    ? Inner[]
+    : readonly Inner[]
+  )
 ;
 
 declare type UnflatArray<
   Inner,
+  Mutable extends boolean = false,
 > = Array<
   Unflat<
     Inner
+    ,
+    Mutable
   >
 >;
