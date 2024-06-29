d1d4a051d27c07a421f4cd4558c01a3c4a55ee1a
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-28 23:42:15 -0700

    upgrade lint v13

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,22 @@
 // eslint.config.js
-// v12.2.0
+// v13.0.0
 import linted from "linted";
 
-export default [
-  ...new linted(
-    {
-      js: [
-        "eslint.config.js",
-        "svelte.config.js",
-      ],
-      ts: [
-        "src/**/*.ts",
-        "vite.config.ts",
-      ],
-      svelte: ["src/**/*.svelte"],
-      html: ["src/**/*.html"],
-      jest: ["src/**/*.spec.ts"],
-      jsonc: ["tsconfig.json"],
-      json: ["package.json"],
-      yml: [".github/workflows/*.yml"],
-    },
-  )
-    .configs,
-];
+export default new linted(
+  {
+    js: [
+      "eslint.config.js",
+      "svelte.config.js",
+    ],
+    ts: [
+      "src/**/*.ts",
+      "vite.config.ts",
+    ],
+    svelte: ["src/**/*.svelte"],
+    html: ["src/**/*.html"],
+    jest: ["src/**/*.spec.ts"],
+    jsonc: ["tsconfig.json"],
+    json: ["package.json"],
+    yml: [".github/workflows/*.yml"],
+  },
+);
