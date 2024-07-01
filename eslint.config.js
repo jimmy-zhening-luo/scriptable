00df66ca63dc57335734ac618e0a0d39619bd80a
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-01 01:09:02 -0700

    upgrade linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,3 @@
 import linted from "linted";
 
-export default linted(
-  {
-    _VERSION: "13.3.0",
-    js: ["*.config.js"],
-    ts: [
-      "src/**/*.ts",
-      "*.config.ts",
-    ],
-    svelte: ["src/**/*.svelte"],
-    html: ["src/**/*.html"],
-    jest: ["src/**/*.spec.ts"],
-    jsonc: ["tsconfig.json"],
-    json: ["package.json"],
-    yml: [".github/workflows/*.yml"],
-  },
-);
+export default linted();
