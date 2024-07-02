f729fba81bba47fe34275f38de99ecfce33d8363
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-02 16:32:29 -0700

    inline: prefer template str, destruct, compact

--- src/inline/Menu.ts
+++ src/inline/Menu.ts
@@ -1,61 +1,38 @@
 export function Menu() {
   "use strict";
 
-  const FOLD = 6;
-  const choices = args
-    .shortcutParameter as readonly string[];
-  const n = choices
-    .length;
-  const pad = "\n"
-    .repeat(
-      Math
-        .max(
-          1,
-          6 - n,
-        ),
-    );
-  const [
-    up,
-    down,
-  ] = [
-    n > FOLD
-      ? ""
-      : pad,
-    pad,
-  ];
-  const inverted = choices
-    .map(
-      choice =>
-        [
-          [
-            up,
-            choice,
-            down,
-          ]
-            .join(
-              "",
-            ),
-          choice,
-        ],
-    );
-  const buttons = inverted
-    .map(
-      (
-        [button],
-      ) =>
-        button,
-    );
+  const choices = args.shortcutParameter as readonly string[];
+  const { length } = choices;
+  const padSize = Math.max(
+    1,
+    6 - length,
+  );
+  const pad = "\n".repeat(padSize);
+  const up = length > 6
+    ? ""
+    : pad;
+  const down = pad;
+  const inverted = choices.map(
+    choice =>
+      [
+        `${up}${choice}${down}`,
+        choice,
+      ],
+  );
+  const buttons = inverted.map(
+    ([button]) =>
+      button,
+  );
 
   return {
     buttons,
-    inverse: Object
-      .fromEntries(
-        inverted,
-      ) as Record<
+    inverse: Object.fromEntries(
+      inverted,
+    ) as Record<
       string
       ,
       string
     >,
-    runner: `__M${n}`,
+    runner: `__M${length}`,
   };
 }
