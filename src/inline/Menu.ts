1e25ef484b00bf1661f7ac20fc203a51189c6c44
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-02 16:06:21 -0700

    inline/menu mv range conditions to shortcut wrapper

--- src/inline/Menu.ts
+++ src/inline/Menu.ts
@@ -1,73 +1,63 @@
 export function Menu() {
   "use strict";
 
-  const range = {
-    max: 9,
-    fold: 6,
-  };
+  const FOLD = 6;
   const choices = args
     .shortcutParameter as Unflat<
     string
   >;
   const n = choices
     .length;
-
-  if (
-    typeof choices === "string"
-    || n < 2
-  )
-    return { choice: choices };
-  else if (
-    n > range
-      .max
-  )
-    return { buttons: choices };
-  else {
-    const pad = "\n"
-      .repeat(
-        Math
-          .max(
-            1,
-            6 - n,
-          ),
-      );
-    const [
-      up,
-      down,
-    ] = [
-      n > range
-        .fold
-        ? ""
-        : pad,
-      pad,
-    ];
-    const inverted = choices
-      .map(
-        choice =>
+  const pad = "\n"
+    .repeat(
+      Math
+        .max(
+          1,
+          6 - n,
+        ),
+    );
+  const [
+    up,
+    down,
+  ] = [
+    n > fold
+      ? ""
+      : pad,
+    pad,
+  ];
+  const inverted = choices
+    .map(
+      choice =>
+        [
           [
-            `${up}${choice}${down}`,
+            up,
             choice,
-          ],
-      );
-    const buttons = inverted
-      .map(
-        (
-          [button],
-        ) =>
-          button,
-      );
+            down,
+          ]
+            .join(
+              "",
+            ),
+          choice,
+        ],
+    );
+  const buttons = inverted
+    .map(
+      (
+        [button],
+      ) =>
+        button,
+    );
 
-    return {
-      buttons,
-      invert: Object
-        .fromEntries(
-          inverted,
-        ) as Record<
-        string
-        ,
-        string
-      >,
-      runner: `__M${n}`,
-    };
-  }
+  return {
+    buttons,
+    inverse: Object
+      .fromEntries(
+        inverted,
+      ) as Record<
+      string
+      ,
+      string
+    >,
+    runner: `__M${n}`,
+  };
 }
