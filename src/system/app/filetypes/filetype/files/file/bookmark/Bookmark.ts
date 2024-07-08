77bc703240804b4cbe514986626bbfd88e93b28f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-07 22:19:31 -0700

    bookmark only str

--- src/system/app/filetypes/filetype/files/file/bookmark/Bookmark.ts
+++ src/system/app/filetypes/filetype/files/file/bookmark/Bookmark.ts
@@ -4,49 +4,36 @@ class Bookmark {
   protected readonly __proto = "Bookmark";
 
   constructor(
-    bookmark:
-      | Bookmark
-      | string
-    ,
+    bookmark: string,
   ) {
     try {
-      if (typeof bookmark !== "string") {
-        this
-          .alias = bookmark
-            .alias;
-        this
-          .path = bookmark
-            .path;
-      }
-      else {
-        const alias = bookmark
-          .trim();
+      const alias = bookmark
+        .trim();
 
-        if (alias.length < 1)
-          throw new SyntaxError(
-            `empty alias`,
-          );
-        else if (
-          !FileManager
+      if (alias.length < 1)
+        throw new SyntaxError(
+          `empty alias`,
+        );
+      else if (
+        !FileManager
+          .local()
+          .bookmarkExists(
+            alias as stringful,
+          )
+      )
+        throw new ReferenceError(
+          `no bookmark matching alias`,
+          { cause: { alias } },
+        );
+      else {
+        this
+          .alias = alias as Alias;
+        this
+          .path = FileManager
             .local()
-            .bookmarkExists(
-              alias as stringful,
-            )
-        )
-          throw new ReferenceError(
-            `no bookmark matching alias`,
-            { cause: { alias } },
-          );
-        else {
-          this
-            .alias = alias as Alias;
-          this
-            .path = FileManager
-              .local()
-              .bookmarkedPath(
-                bookmark,
-              ) as Stringify<Rootpath>;
-        }
+            .bookmarkedPath(
+              bookmark,
+            ) as Stringify<Rootpath>;
       }
     }
     catch (e) {
