e4c6711035e636fe5b0bce36458b3acdb7d25d57
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-11 10:07:57 -0700

    --clean i don't like it

--- src/common/validator/base/string/valid/CleanString.ts
+++ src/common/validator/base/string/valid/CleanString.ts
@@ -18,14 +18,10 @@ class CleanString<Validator extends string> extends boundString<
       min?: Positive<fint>;
       max?: Positive<int>;
     } = {},
-    cleanOptions: Parameters<typeof CleanString.clean>[1] = {},
   ) {
     try {
       super(
-        CleanString.clean(
-          string,
-          cleanOptions,
-        ),
+        string,
         charset,
         filter,
         min,
@@ -35,102 +31,6 @@ class CleanString<Validator extends string> extends boundString<
     catch (e) {
       throw new EvalError(
         `CleanString: ctor`,
-        { cause: e },
-      );
-    }
-  }
-
-  private static clean(
-    string: string,
-    {
-      toLower = false,
-      trim = false,
-      trimLeadingExcept = false,
-      trimTrailingExcept = false,
-      trimLeading = [],
-      trimTrailing = [],
-    }: {
-      toLower?: boolean;
-      trim?: boolean;
-      trimLeadingExcept?: boolean;
-      trimTrailingExcept?: boolean;
-      trimLeading?: readonly stringful[];
-      trimTrailing?: readonly stringful[];
-    },
-  ) {
-    try {
-      return this.trimEdge(
-        this.trimEdge(
-          string[
-            toLower
-              ? "toLowerCase"
-              : "toString"
-          ]()[
-            trim
-              ? "trim"
-              : "toString"
-          ](),
-          "leading",
-          trimLeading,
-          trimLeadingExcept,
-        ),
-        "trailing",
-        trimTrailing,
-        trimTrailingExcept,
-      );
-    }
-    catch (e) {
-      throw new EvalError(
-        `CleanString: clean`,
-        { cause: e },
-      );
-    }
-  }
-
-  private static trimEdge(
-    string: string,
-    edge:
-      | "leading"
-      | "trailing",
-    wordsToTrim: readonly stringful[],
-    trimExcept: boolean,
-  ) {
-    try {
-      let trimmed = string;
-      const lookFn = edge === "leading"
-        ? "startsWith"
-        : "endsWith";
-      const slicer = edge === "leading"
-        ? trimExcept
-          ? () =>
-              [1]
-          : (word: string) =>
-              [word.length]
-        : trimExcept
-          ? () =>
-              [
-                0,
-                -1,
-              ]
-          : (word: string) =>
-              [
-                0,
-                0 - word.length,
-              ];
-
-      wordsToTrim
-        .forEach(
-          word => {
-            while (trimExcept !== trimmed[lookFn](word))
-              trimmed = trimmed.slice(...slicer(word));
-          },
-        );
-
-      return trimmed;
-    }
-    catch (e) {
-      throw new EvalError(
-        `CleanString: trimEdge`,
         { cause: e },
       );
     }
