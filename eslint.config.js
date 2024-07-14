50e8e0e251f4e1c16f5c0e4de648e85fe9fb844b
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-13 21:07:30 -0700

    patch lint, remove override

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,3 @@
 import linted from "linted";
 
-export default linted(
-  {},
-  {
-    overrideTs: {
-      "@typescript-eslint/array-type": [
-        "error",
-        {
-          "default": "array",
-          readonly: "array",
-        },
-      ],
-    },
-  },
-);
+export default linted();
