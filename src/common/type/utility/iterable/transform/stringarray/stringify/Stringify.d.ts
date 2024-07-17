a38222efbefaba802e1661908c38245401e9e707
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-16 23:04:09 -0700

    unnecessary stringify constraints

--- src/common/type/utility/iterable/transform/stringarray/stringify/Stringify.d.ts
+++ src/common/type/utility/iterable/transform/stringarray/stringify/Stringify.d.ts
@@ -1,9 +1,9 @@
 declare type Stringify<C> = C extends Record<"string", infer S extends string>
-  ? [S] extends [string]
-      ? S
-      : never
+  ? S extends string
+    ? S
+    : never
   : C extends Record<"toString", infer Fn>
-    ? [Fn] extends [()=> string]
-        ? ReturnType<Fn>
-        : never
+    ? Fn extends ()=> string
+      ? ReturnType<Fn>
+      : never
     : never;
