eeb9009e31c443ccb81bc0f98d24b30010727232
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-17 16:56:29 -0700

    errrant test type

--- src/common/type/utility/iterable/transform/stringarray/stringify/Stringify.d.ts
+++ src/common/type/utility/iterable/transform/stringarray/stringify/Stringify.d.ts
@@ -3,5 +3,3 @@ declare type Stringify<C> = C extends Record<"string", infer S extends string>
   : C extends Record<"toString", infer Fn extends ()=> string>
     ? ReturnType<Fn>
     : never;
-
-type GG = Stringify<Filepath<1>>;
