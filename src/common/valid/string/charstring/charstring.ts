344518d311ee22346c142e6d817bea4e2582ae28
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-27 20:51:34 -0700

    deprecate charset, it was really just a function wrapped in a class, mv all capabilities into charstring itself

--- src/common/valid/string/charstring/charstring.ts
+++ src/common/valid/string/charstring/charstring.ts
@@ -9,7 +9,7 @@ class charstring<Validator extends string> {
       min = 1 as Positive<fint>,
       max = Infinity as Positive<int>,
     }: {
-      filter?: CharFilter;
+      filter?: "include" | "exclude";
       min?: Positive<fint>;
       max?: Positive<int>;
     } = {},
@@ -22,12 +22,11 @@ class charstring<Validator extends string> {
       else if (length < min)
         throw new TypeError(`String is too short`);
       else
-        this.string = new charstring
-          .charset(
-            filter,
-            chars,
-          )
-          .of<typeof this["string"]>(string);
+        this.string = this.is(
+          string,
+          filter,
+          chars,
+        );
     }
     catch (e) {
       throw new Error(
@@ -37,15 +36,49 @@ class charstring<Validator extends string> {
     }
   }
 
-  private static get charset() {
+  public is(
+    string: string,
+    filter: "include" | "exclude",
+    chars: char[],
+  ) {
     try {
-      return importModule<typeof charset>(
-        "charset/charset",
-      );
+      if (this[filter](
+        string,
+        chars,
+      ))
+        return string;
+      else
+        throw new TypeError(`String has disallowed chars`);
     }
     catch (e) {
-      throw new ReferenceError(
-        `charstring: import charset`,
+      throw new Error(
+        `charset: allows`,
+        { cause: e },
+      );
+    }
+  }
+
+  protected include(string: string, chars: char[]): string is this["string"] {
+    try {
+      return ([...string] as char[])
+        .every(stringchar => chars.includes(stringchar));
+    }
+    catch (e) {
+      throw new Error(
+        `charset: include`,
+        { cause: e },
+      );
+    }
+  }
+
+  protected exclude(string: string, chars: char[]): string is this["string"] {
+    try {
+      return chars
+        .every(char => !string.includes(char));
+    }
+    catch (e) {
+      throw new Error(
+        `charset: exclude`,
         { cause: e },
       );
     }
