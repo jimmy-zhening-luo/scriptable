28ad45b757837fc0a07b227ba61ae4b64eae58bf
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-31 16:21:39 -0700

    Stringify: support stringifier functions too

--- src/common/type/utility/iterable/transform/stringarray/stringify/Stringify.d.ts
+++ src/common/type/utility/iterable/transform/stringarray/stringify/Stringify.d.ts
@@ -4,8 +4,24 @@ declare type Stringify<C> = C extends abstract new (...args: infer A)=> infer O
     : O extends { toString: ()=> infer S extends string }
       ? S
       : never
-  : C extends { string: infer S extends string }
+  : C extends ((...args: infer A)=> infer S extends string)
     ? S
-    : C extends { toString: ()=> infer S extends string }
+    : C extends { string: infer S extends string }
       ? S
-      : never;
+      : C extends { toString: ()=> infer S extends string }
+        ? S
+        : never;
+
+declare namespace Stringify {
+  export type T = Stringify<CharString<"OK-Instance">>;
+  export type T0 = Stringify<CharStringCtor>;
+  export type T1 = Stringify<charstringfunc<"OK-Function">>;
+
+  type CharStringCtor = abstract new ()=> CharString<"OK-Constructor">;
+
+  interface CharString<Validator extends string> {
+    readonly string: valid<stringful, [Validator, "string"]>;
+  }
+
+  type charstringfunc<Validator extends string> = ()=> valid<stringful, [Validator, "string"]>;
+}
