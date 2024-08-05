221c3b54c20f6d7d64e1d8f19434fb317e7710ed
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-08-04 17:16:58 -0700

    lol

--- src/system/app/error/errorlike/index.d.ts
+++ src/system/app/error/errorlike/index.d.ts
@@ -1,6 +1,3 @@
 declare type ErrorLike<Real extends boolean = false> = True<Real> extends never
-  ?
-  | Error
-  | { cause?: ErrorLike }
-  | Unflat<primitive>
+  ? Error | { cause?: ErrorLike } | Unflat<primitive>
   : Error;
