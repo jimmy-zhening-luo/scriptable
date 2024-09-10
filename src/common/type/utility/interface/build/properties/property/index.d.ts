55aa186211be3acb358c2439432e11e3f5d898f8
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-09-10 14:36:56 -0700

    bump pkg 20.17.8; update ts 5.6, eslint 9.10, lint 16.5; relint project

--- src/common/type/utility/interface/build/properties/property/index.d.ts
+++ src/common/type/utility/interface/build/properties/property/index.d.ts
@@ -1,7 +1,7 @@
 declare type Property<K extends string, OK extends string, V> = literalful<K> extends never
   ? Particord<OK, V>
   : Recordful<K, V>
-  & (
+    & (
     literalful<OK> extends never
       ? object
       : Particord<OK, V>
