5b4461ee85939f164d074864aac1f22434d7600d
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-09-15 23:49:59 -0700

    strings: s/filter/comparator

--- src/common/valid/string/index.ts
+++ src/common/valid/string/index.ts
@@ -1,34 +1,33 @@
 declare type vstring<V extends string> = valid<stringful, [V, "string"]>;
-type Filter = "include" | "exclude";
 
 function vstring<V extends string>(
   string: string,
   chars: char[],
   {
-    filter = "exclude",
+    comparator = "exclude",
     min = 1 as Positive<fint>,
     max = Infinity as Positive<int>,
   }: {
-    filter?: Filter;
+    comparator?: "include" | "exclude";
     min?: Positive<fint>;
     max?: Positive<int>;
   } = {},
 ): vstring<V> {
   try {
-    const filters = {
+    const comparators = {
       include(string: string, chars: char[]): string is vstring<V> {
         return [...string].every(s => chars.includes(s as char));
       },
       exclude(string: string, chars: char[]): string is vstring<V> {
         return chars.every(c => !string.includes(c));
       },
-    } satisfies Record<Filter, (string: string, chars: char[]) => string is vstring<V>>;
+    } satisfies Record<"include" | "exclude", (string: string, chars: char[]) => string is vstring<V>>;
 
     if (min > max)
       throw new RangeError(`Bad args: min > max`);
     else if (string.length < min)
       throw new TypeError(`String is too short`);
-    else if (!filters[filter](string, chars))
+    else if (!comparators[comparator](string, chars))
       throw new TypeError(`String has disallowed chars`);
     else
       return string;
