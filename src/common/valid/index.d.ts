e01b39c531271f0431be2cde18be3a037ded0260
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-09-17 22:35:25 -0700

    common/valid: s/Validators/V, -unnecessary primitive type constraint
    (valid anything whatever)

--- src/common/valid/index.d.ts
+++ src/common/valid/index.d.ts
@@ -1,5 +1,7 @@
 declare const v: unique symbol;
-declare type valid<T extends primitive, Validators extends readonly string[]> =
-  & ([T] extends [primitive] ? T : never)
-  & (Chain<Validators, ":", true> extends never ? never : { [v]: Chain<Validators, ":", true> })
+declare type valid<T, V extends readonly string[]> =
+  & T
+  & (Chain<V, ":", true> extends never
+    ? never
+    : { [v]: Chain<V, ":", true> })
 ;
