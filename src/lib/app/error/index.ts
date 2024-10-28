2cadb4a630d734976ed79e4115ca8421791ea2ee
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2024-10-28 15:27:08 -0700

    correctly handle unknown throws

--- src/lib/app/error/index.ts
+++ src/lib/app/error/index.ts
@@ -1,12 +1,12 @@
-function error(error: Error) {
+function error(error: unknown) {
   const stringify = (e: unknown): string => typeof e === "object" && e !== null
     ? Array.isArray(e)
       ? `[${e.map(i => stringify(i)).join(", ")}]`
       : `{ ${Object.entries(e).map(([k, v]) => `${k}: ${stringify(v)}`).join(", ")} }`
     : String(e),
-  errors = [error] as Arrayful<ErrorLike>;
+  errors = [error] as Arrayful<unknown>;
 
-  while (typeof errors[0] === "object" && "cause" in errors[0])
+  while (typeof errors[0] === "object" && errors[0] !== null && "cause" in errors[0])
     errors.unshift(errors[0].cause ?? "null");
 
   const messages = errors.map(
