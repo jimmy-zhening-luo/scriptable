06bc8ab27407dff662844a7e8541420b7e98aed1
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-10-31 02:31:45 -0700

    tests: engine

---
+++ src/apps/method/search/engines/engine/index.spec.ts
@@ -0,0 +1,95 @@
+import { expect } from "chai";
+import SearchEngine from "./index.js";
+import type Query from "../../query";
+
+describe("SearchEngine", function () {
+  class TestEngine extends SearchEngine<"find"> {
+    protected optional(query: Query) {
+      return {
+        reversed: [...query.natural]
+          .reverse()
+          .join(""),
+      };
+    }
+  }
+
+  // items near me
+  // em raen smeti
+
+  const query: Query = {
+    key: "foo" as stringful,
+    terms: ["items", "near", "me"] as stringful[],
+    natural: "items near me",
+    engine: { find: "Tester" },
+    toString() {
+      return `${this.key} ${this.natural}` as stringful;
+    },
+  },
+  engine = new TestEngine("find", "Tester"),
+  outEngine = new TestEngine("find", "TesterWithOutput", true);
+
+  describe("shape", function () {
+    it("is object", function () {
+      expect(engine)
+        .an("object");
+    });
+  });
+  describe("property", function () {
+    it("app is engine type", function () {
+      expect(engine["app"])
+        .a("string")
+        .that.equals("find");
+      expect(outEngine["app"])
+        .a("string")
+        .that.equals("find");
+    });
+    it("engine has assigned name", function () {
+      expect(engine["engine"])
+        .a("string")
+        .that.equals("Tester");
+      expect(outEngine["engine"])
+        .a("string")
+        .that.equals("TesterWithOutput");
+    });
+    it("output is Null<true>", function () {
+      expect(engine["output"])
+        .a("null")
+        .that.equals(null);
+      expect(outEngine["output"])
+        .a("boolean")
+        .that.equals(true);
+    });
+  });
+  describe("output", function () {
+    it("is object", function () {
+      expect(engine.resolve(query))
+        .an("object");
+      expect(outEngine.resolve(query))
+        .an("object");
+    });
+    it("has all keys", function () {
+      expect(engine.resolve(query))
+        .has.all.keys("app", "find", "output", "action", "reversed");
+      expect(outEngine.resolve(query))
+        .has.all.keys("app", "find", "output", "action", "reversed");
+    });
+    it("has expected values", function () {
+      expect(engine.resolve(query))
+        .deep.equals({
+          app: "find",
+          find: "Tester",
+          output: null,
+          action: "items near me",
+          reversed: "em raen smeti",
+        });
+      expect(outEngine.resolve(query))
+        .deep.equals({
+          app: "find",
+          find: "TesterWithOutput",
+          output: true,
+          action: "items near me",
+          reversed: "em raen smeti",
+        });
+    });
+  });
+});
