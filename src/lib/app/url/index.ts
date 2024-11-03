d659df96556de69094dcf108b7b1396176ae039f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-03 00:37:16 -0700

    url: 1-liner parse

--- src/lib/app/url/index.ts
+++ src/lib/app/url/index.ts
@@ -1,25 +1,21 @@
 function url(string: string) {
-  const parse = (string: string, tryHttp = false) => {
-    const {
-      scheme = "",
-      host = "",
-      path = "",
-      query = "",
-      fragment = "",
-    } = (/^(?:(?<scheme>[^:/?#]+):)?(?:\/\/(?<host>[^/?#]*))?(?<path>[^?#]*)(?:\?(?<query>[^#]*))?(?:#(?<fragment>.*))?/u)
-      .exec(`${tryHttp ? "https" : ""}${string}`)
-      ?.groups ?? {};
-
-    return scheme.length > 0 && (!tryHttp || host.length > 0 || path.length > 0)
-      ? {
-          scheme: scheme.toLocaleLowerCase(),
-          host: host.toLocaleLowerCase(),
-          path,
-          query,
-          fragment,
-        }
-      : null;
-  },
+  const parse = (string: string, tryHttp = false) => (({
+    scheme = "",
+    host = "",
+    path = "",
+    query = "",
+    fragment = "",
+  }) => scheme.length > 0 && (!tryHttp || host.length > 0 || path.length > 0)
+    ? {
+        scheme: scheme.toLocaleLowerCase(),
+        host: host.toLocaleLowerCase(),
+        path,
+        query,
+        fragment,
+      }
+    : null)((/^(?:(?<scheme>[^:/?#]+):)?(?:\/\/(?<host>[^/?#]*))?(?<path>[^?#]*)(?:\?(?<query>[^#]*))?(?:#(?<fragment>.*))?/u)
+    .exec(`${tryHttp ? "https" : ""}${string}`)
+    ?.groups ?? {}),
   parts = parse(string) ?? parse(string, true);
 
   if (parts === null)
