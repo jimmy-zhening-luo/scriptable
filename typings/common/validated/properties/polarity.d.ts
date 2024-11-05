12c8bf3b58f9fc9e3a2ffa4e1c7f8d4486a82edd
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-04 22:24:18 -0800

    lint

--- typings/common/validated/properties/polarity.d.ts
+++ typings/common/validated/properties/polarity.d.ts
@@ -2,4 +2,4 @@ declare const polarity: unique symbol;
 declare type polarity<
   P extends "-" | "+" = "+",
   NumberType extends number = numberful,
-> = NumberType & { [polarity]: P; };
+> = NumberType & { [polarity]: P };
