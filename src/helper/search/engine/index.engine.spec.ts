e8d3e5510d162af0174997301bf997ce3d94b63a
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2024-11-10 02:06:02 -0800

    fix text

--- src/helper/search/engine/index.engine.spec.ts
+++ src/helper/search/engine/index.engine.spec.ts
@@ -2,7 +2,7 @@ import SearchEngine from "./index.js";
 import type Query from "../query.js";
 
 export default class TestEngine extends SearchEngine<"find"> {
-  protected optional(query: Query) {
+  protected override optional(query: Query) {
     return {
       reversed: [...query.natural]
         .reverse()
