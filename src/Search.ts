e03552992d021f07ddad81faab31759676e0b5f5
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-15 21:06:49 -0800

    lint

--- src/Search.ts
+++ src/Search.ts
@@ -66,7 +66,7 @@ class Search extends Shortcut<
           : "shortcut" in entry
             ? new Engine("shortcut", entry.shortcut, entry.output)
             : new Engine("find", entry.find),
-    { 
+    {
       key,
       terms,
       question,
