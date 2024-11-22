0fe7070dc4915b81e649d667aea22bd73a9d06e0
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-21 21:03:32 -0800

    - number cast, bad idea

--- src/types/utility/primitive/cast/number.d.ts
+++
@@ -1,9 +0,0 @@
-declare type ToNumber<Value extends string> = literalful<Value> extends never
-  ? never
-  : literalful<Value> extends literalful<Exclude<literalful<Value>, "0" | "-0" >>
-    ? literalful<Value> extends `${infer Number extends number}`
-      ? Numeric<Number>
-      : never
-    : literalful<Exclude<literalful<Value>, "0" | "-0" >> extends `${infer Number extends number}`
-      ? Numeric<0 | Number>
-      : never;
