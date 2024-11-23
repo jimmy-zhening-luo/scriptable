f55cd49cc708dec4650a4622108c4c202261cbca
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-23 01:21:13 -0800

    arrayn bug fix (valid & invalid nums coerce to element[])

--- src/types/utility/iterable/array-n.d.ts
+++ src/types/utility/iterable/array-n.d.ts
@@ -3,17 +3,22 @@ declare type ArrayN<N extends number = 0, Element = string> = [Element] extends 
   ? never
   : [N] extends [never]
       ? never
-      : [N] extends [number]
-          ? [number] extends [N]
-              ? [...Element[]]
-              : `${N}` extends `${0}` | `-${string}` | `${string}.${string}`
-                ? [...Element[]]
-                : ArrayN.Construct<N, Element>
-          : never;
+      : Extract<`${N}`, `-${string}` | `${string}.${string}`> extends never
+        ? [0] extends [N]
+            ? Element[]
+            : ArrayN.Construct<N, Element>
+        : Element[];
 
 declare namespace ArrayN {
-  export type Construct<N extends number, Element, H extends Element[] = []> = H["length"] extends N
+  export type Construct<
+    N extends number,
+    Element,
+    H extends Element[] = [],
+  > = H["length"] extends N
     ? [...H, ...Element[]]
-    : Construct<N, Element, [...H, Element]>;
-
+    : Construct<
+      N,
+      Element,
+      [...H, Element]
+    >;
 }
--- src/types/utility/iterable/array-n.spec.d.ts
+++ src/types/utility/iterable/array-n.spec.d.ts
@@ -1,47 +1,73 @@
 declare namespace ArrayN {
+  export type Bound<
+    Case extends number,
+    Max extends number = 0,
+  > = ArrayN<Case> extends ArrayN<Max>
+    ? ArrayN<Case> extends [string, ...ArrayN<Max>]
+      ? never
+      : true
+    : never;
   export type Result = 0 | Test<{
     T: [
-      ArrayN<1> extends [unknown, ...unknown[]] ? true : never,
-      ArrayN<1.00000> extends ArrayN<1> ? true : never,
-      ArrayN<1 | 2> extends ArrayN<1> ? true : never,
-      ArrayN<1 | 2 | 3> extends ArrayN<1> ? true : never,
-      ArrayN<2> extends ArrayN<2> ? true : never,
-      ArrayN<2 | 3> extends ArrayN<2> ? true : never,
-      // string[]:
-      ArrayN extends unknown[] ? ArrayN extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<number> extends unknown[] ? ArrayN<number> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<typeof NaN> extends unknown[] ? ArrayN<typeof NaN> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<typeof Infinity> extends unknown[] ? ArrayN<typeof Infinity> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<0 | 1> extends unknown[] ? ArrayN<0 | 1> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<0 | 2> extends unknown[] ? ArrayN<0 | 2> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<0 | 1 | 2> extends unknown[] ? ArrayN<0 | 1 | 2> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<-1> extends unknown[] ? ArrayN<-1> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<1.1> extends unknown[] ? ArrayN<1.1> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<-1.1> extends unknown[] ? ArrayN<-1.1> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<-1.00000> extends unknown[] ? ArrayN<-1.00000> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<9.999999999> extends unknown[] ? ArrayN<9.999999999> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<-1.1 | 3.4> extends unknown[] ? ArrayN<-1.1 | 3.4> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<-1.1 | -3.4> extends unknown[] ? ArrayN<-1.1 | -3.4> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<-1 | 3.4> extends unknown[] ? ArrayN<-1 | 3.4> extends [unknown, ...unknown[]] ? never : true : never,
-      ArrayN<-1 | -3.4> extends unknown[] ? ArrayN<-1 | -3.4> extends [unknown, ...unknown[]] ? never : true : never,
+      ArrayN<3> extends [string, string, string, ...string[]]
+        ? ArrayN<3> extends [string, string, string, string, ...string[]]
+          ? never
+          : true
+        : never,
+      ArrayN<2> extends [string, string, ...string[]] ? ArrayN<2> extends ArrayN<3> ? never : true : never,
+      ArrayN<1> extends [string, ...string[]] ? ArrayN<1> extends ArrayN<2> ? never : true : never,
+      Bound<1.0, 1>,
+      Bound<0>,
+      Bound<-0>,
+      Bound<-1>,
+      Bound<-1.00000>,
+      Bound<-2>,
+      Bound<-2.0>,
+      Bound<-2.1>,
+      Bound<typeof NaN>,
+      Bound<number>,
+      Bound<typeof Infinity>,
+      Bound<9.999999999>,
+      Bound<0 | 1>,
+      Bound<0 | 2>,
+      Bound<0 | 1 | 2>,
+      Bound<-1.1 | 3.4>,
+      Bound<-1.1 | -3.4>,
+      Bound<-1 | 3.4>,
+      Bound<-1 | -3.4>,
+      Bound<2 | -1>,
+      Bound<2 | 1.1>,
+      Bound<2 | -1.1>,
+      Bound<2 | -1.00000>,
+      Bound<2 | 9.999999999>,
+      unknown[] extends ArrayN<0, unknown> ? true : never,
+      (string | number)[] extends ArrayN<0, string | number> ? true : never,
 
-      // BUG: should be unknown[] (actual: [unknown, unknown, ...unknown[]]):
-      // ArrayN<2 | -1> extends unknown[] ? ArrayN<2 | -1> extends [unknown, ...unknown[]] ? never : true : never,
-      // ArrayN<2 | 1.1> extends unknown[] ? ArrayN<2 | 1.1> extends [unknown, ...unknown[]] ? never : true : never,
-      // ArrayN<2 | -1.1> extends unknown[] ? ArrayN<2 | -1.1> extends [unknown, ...unknown[]] ? never : true : never,
-      // ArrayN<2 | -1.00000> extends unknown[] ? ArrayN<2 | -1.00000> extends [unknown, ...unknown[]] ? never : true : never,
-      // ArrayN<2 | 9.999999999> extends unknown[] ? ArrayN<2 | 9.999999999> extends [unknown, ...unknown[]] ? never : true : never,
-      // End BUG: should be unknown[] (actual: [unknown, unknown, ...unknown[]])
-
-      // Error (string[]):
-      // ArrayN<0> extends unknown[] ? ArrayN extends [unknown, ...unknown[]] ? never : true : never,
-      // ArrayN<-0> extends unknown[] ? ArrayN extends [unknown, ...unknown[]] ? never : true : never,
-      // ArrayN<0 | number> extends unknown[] ? ArrayN extends [unknown, ...unknown[]] ? never : true : never,
-      // ArrayN<1 | number> extends unknown[] ? ArrayN extends [unknown, ...unknown[]] ? never : true : never,
-      // ArrayN<2 | number> extends unknown[] ? ArrayN extends [unknown, ...unknown[]] ? never : true : never,
+      // Error:
+      // Bound<-2.1 | number>,
+      // Bound<-2.0 | number>,
+      // Bound<-2 | number>,
+      // Bound<-1 | number>,
+      // Bound<0 | number>,
+      // Bound<1 | number>,
+      // Bound<2 | number>,
+      // Bound<2.0 | number>,
+      // Bound<2.1 | number>,
+      // Bound<2.1 | number>,
+      // unknown[] extends ArrayN<0, unknown | string> ? true : never,
     ];
     F: [
-      ArrayN<never>,
+      ArrayN<never, never>,
+      ArrayN<number, never>,
+      ArrayN<2.1, never>,
+      ArrayN<2.0, never>,
+      ArrayN<2, never>,
+      ArrayN<1, never>,
+      ArrayN<0, never>,
+      ArrayN<-1, never>,
+      ArrayN<-2, never>,
+      ArrayN<-2.0, never>,
+      ArrayN<-2.1, never>,
     ];
   }>;
 }
