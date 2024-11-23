2485b4378e29bbaaea69e1628a826bda7741d1b6
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-23 03:31:56 -0800

    type: properties: -limit (unused)

--- src/types/utility/record/alias/properties.d.ts
+++ src/types/utility/record/alias/properties.d.ts
@@ -1,6 +1,5 @@
 declare type Field<K extends string, OK extends string = never> = Property<K, OK, string>;
 declare type Flag<OK extends string, K extends string = never> = Property<K, OK, boolean>;
-declare type Limit<K extends string, OK extends string = never> = Property<K, OK, Record<"min" | "max", number>>;
 declare type List<K extends string, OK extends string = never> = Property<K, OK, readonly string[]>;
 declare type Listish<K extends string, OK extends string = never> = Property<K, OK, Unflat>;
 declare type Scalar<K extends string, OK extends string = never> = Property<K, OK, number>;
