eb368eecda95cb39cf98684323cbfe9cca1233bf
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-23 22:35:40 -0800

    Link: return stringful, use stringfuls func to check input

--- src/Copy File Link.ts
+++ src/Copy File Link.ts
@@ -2,20 +2,19 @@
 // share-sheet-inputs: file-url;
 import Share from "./lib/share";
 
-class FileLink extends Share<string> {
+class FileLink extends Share<stringful> {
   protected readonly type = "fileURLs";
 
   protected runtime() {
-    const { input: files = [] as string[] } = this;
+    const { input: files = [] } = this,
+    links = FileLink
+      .stringfuls(files, "Shared Files")
+      .map((path: stringful) => `shareddocuments://${encodeURI(path)}` as stringful) as ReturnType<typeof FileLink.stringfuls<typeof files>>,
+    response = (links satisfies Arrayful<stringful>).join("\n") as stringful;
 
-    if (files.length < 1)
-      throw new RangeError("No folders/files shared");
+    Pasteboard.copyString(response);
 
-    const links = files.map(path => `shareddocuments://${encodeURI(path)}`).join("\n");
-
-    Pasteboard.copyString(links);
-
-    return links;
+    return response;
   }
 }
 
