7c4f0cbe231ca6efb9ae3945977c3668a995662d
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-01-06 12:12:06 -0800

    retain https

--- src/Link.ts
+++ src/Link.ts
@@ -32,7 +32,7 @@ class Link extends Shortcut<
       path,
       query,
       fragment,
-    } = url(input, true),
+    } = url(input),
     host = ((host: ReturnType<typeof url>["host"]) => (headless => hosts.swap[headless] ?? headless)(!host.startsWith("www.") || hosts.preserve.includes(host) ? host : host.slice(4) as typeof host))(_host),
     [
       include = null,
