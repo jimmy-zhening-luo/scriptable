5b3fece7c2768ac0734bdc83becdb86544634a14
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-01-24 23:30:42 -0800

    hm

--- src/Link.ts
+++ src/Link.ts
@@ -3,13 +3,13 @@ import Shortcut from "./lib";
 import Url from "./lib/object/url";
 import type { LinkSetting } from "./interface/link";
 
-function process(host: ReturnType<typeof url>["host"], path: string) {
+function process(host: Url["host"], path: string) {
   const processors = {
     "amazon.com": path => (([, pid = null]) => pid === null ? path : `/dp/${(pid.split("/") as Arrayful)[0]}`)(path.split("/dp/")),
     "dropbox.com": path => !path.startsWith("/scl/fi/") ? path : (nodes => nodes.length < 4 ? path : nodes.slice(0, 4).join("/"))(path.split("/")),
     "linkedin.com": path => path.startsWith("/mwlite/") ? path.slice(7) : path,
     "reddit.com": path => (nodes => nodes.length < 6 || nodes[3] !== "comments" ? path : nodes.slice(0, nodes[5] === "comment" ? 7 : 5).join("/"))(path.split("/")),
-  } satisfies Record<string, (path: string) => string> as Record<ReturnType<typeof url>["host"], (path: string) => string>;
+  } satisfies Record<string, (path: string) => string> as Url["host"], (path: string) => string>;
 
   return processors[host]?.(path) ?? path;
 }
@@ -33,7 +33,7 @@ class Link extends Shortcut<
       query,
       fragment,
     } = new Url(input),
-    host = ((host: ReturnType<typeof url>["host"]) => (headless => hosts.swap[headless] ?? headless)(!host.startsWith("www.") || hosts.preserve.includes(host) ? host : host.slice(4) as typeof host))(_host),
+    host = ((host: Url["host"]) => (headless => hosts.swap[headless] ?? headless)(!host.startsWith("www.") || hosts.preserve.includes(host) ? host : host.slice(4) as typeof host))(_host),
     [
       include = null,
       exclude = null,
@@ -41,7 +41,7 @@ class Link extends Shortcut<
 
     return (({
       scheme, host, path, query, fragment,
-    }: ReturnType<typeof url>) => `${scheme}${scheme === "" ? "" : "://"}${host}${path === "/" ? "" : path}${query === "" ? "" : "?"}${query}${fragment}` as stringful)({
+    }: Url) => `${scheme}${scheme === "" ? "" : "://"}${host}${path === "/" ? "" : path}${query === "" ? "" : "?"}${query}${fragment}` as stringful)({
       scheme,
       host,
       path: process(host, path),
