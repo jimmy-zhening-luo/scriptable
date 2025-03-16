b4301ab10afde4b87f9451650edd8498bdf1492c
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-03-15 18:52:40 -0700

    Link.ts: mv link processor helper

--- src/Link.ts
+++ src/Link.ts
@@ -1,20 +1,8 @@
 // icon-color: light-gray; icon-glyph: link;
 import Shortcut from "./lib";
 import Url from "./lib/object/url";
+import process from "./helper/link";
 import type { LinkSetting } from "./interface/Link";
-
-function process(host: Url["host"], path: string) {
-  const processors = {
-    "amazon.com": path => (([, pid = null]) => pid === null ? path : `/dp/${(pid.split("/") as Arrayful)[0]}`)(path.split("/dp/")),
-    "dhl.com": path => path.endsWith("/tracking.html") ? "/tracking.html" : path.startsWith("/orders/") && path.includes("/details/") ? `/tracking.html?tracking-id=${path.split("/")[4] ?? ""}` : path,
-    "dropbox.com": path => !path.startsWith("/scl/fi/") ? path : (nodes => nodes.length < 4 ? path : nodes.slice(0, 4).join("/"))(path.split("/")),
-    "linkedin.com": path => path.startsWith("/mwlite/") ? path.slice(7) : path,
-    "reddit.com": path => (nodes => nodes.length < 6 || nodes[3] !== "comments" ? path : nodes.slice(0, nodes[5] === "comment" ? 7 : 5).join("/"))(path.split("/")),
-    "tools.usps.com": path => path.startsWith("/go/Track") ? "/go/TrackConfirmAction" : path,
-  } satisfies Record<string, (path: string) => string> as Record<Url["host"], (path: string) => string>;
-
-  return processors[host]?.(path) ?? path;
-}
 
 class Link extends Shortcut<
   string,
@@ -48,7 +36,10 @@ class Link extends Shortcut<
         url.replaceParam(find, replace);
 
     const {
-      scheme, path, query, fragment,
+      scheme,
+      path,
+      query,
+      fragment,
     } = url;
 
     return ((
---
+++ src/helper/link.ts
@@ -0,0 +1,14 @@
+import type Url from "./lib/object/url";
+
+export default function (host: Url["host"], path: string) {
+  const processors = {
+    "amazon.com": path => (([, pid = null]) => pid === null ? path : `/dp/${(pid.split("/") as Arrayful)[0]}`)(path.split("/dp/")),
+    "dhl.com": path => path.endsWith("/tracking.html") ? "/tracking.html" : path.startsWith("/orders/") && path.includes("/details/") ? `/tracking.html?tracking-id=${path.split("/")[4] ?? ""}` : path,
+    "dropbox.com": path => !path.startsWith("/scl/fi/") ? path : (nodes => nodes.length < 4 ? path : nodes.slice(0, 4).join("/"))(path.split("/")),
+    "linkedin.com": path => path.startsWith("/mwlite/") ? path.slice(7) : path,
+    "reddit.com": path => (nodes => nodes.length < 6 || nodes[3] !== "comments" ? path : nodes.slice(0, nodes[5] === "comment" ? 7 : 5).join("/"))(path.split("/")),
+    "tools.usps.com": path => path.startsWith("/go/Track") ? "/go/TrackConfirmAction" : path,
+  } satisfies Record<string, (path: string) => string> as Record<Url["host"], (path: string) => string>;
+
+  return processors[host]?.(path) ?? path;
+}
