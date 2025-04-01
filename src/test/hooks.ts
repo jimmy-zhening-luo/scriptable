0444687746402dc1c61040f2fbb771c8603ca94d
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-04-01 00:35:13 -0700

    hooks.ts: + logging

--- src/test/hooks.ts
+++ src/test/hooks.ts
@@ -2,9 +2,15 @@ import * as Synthetics from "./synthetics";
 
 export const mochaHooks = {
   beforeAll() {
-    global.args = Synthetics.args;
-    global.FileManager = Synthetics.FileManager;
-    global.Notification = Synthetics.Notification;
-    global.DateFormatter = Synthetics.DateFormatter;
+    try {
+      console.log("Mocha: global setup hook begin");
+      global.args = Synthetics.args;
+      global.FileManager = Synthetics.FileManager;
+      global.Notification = Synthetics.Notification;
+      global.DateFormatter = Synthetics.DateFormatter;
+      console.log("Mocha: global setup hook end");
+    } catch (e) {
+      throw new EvalError("Mocha: failed global setup", { cause: e });
+    }
   },
 };
