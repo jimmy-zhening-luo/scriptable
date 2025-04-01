417759eb2f7826e1ab94c571e17bf9f7858b59c2
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-04-01 00:46:26 -0700

    mv setup inside test

--- src/app/base/file/index.spec.ts
+++ src/app/base/file/index.spec.ts
@@ -2,17 +2,17 @@ import { should } from "chai";
 import type File from ".";
 
 should();
-const { "default": file } = await (import(".") as Promise<Record<"default", typeof File>>)
-  .catch((e: unknown) => {
-    throw new EvalError(
-      "Mocha: failed to load `file` module",
-      { cause: e },
-    );
-  })
-  .finally(() => console.log("Mocha: file: module loader executed"));
 
 describe("File", function () {
-  const synthetic = new file(
+  const { "default": file } = await (import(".") as Promise<Record<"default", typeof File>>)
+    .catch((e: unknown) => {
+      throw new EvalError(
+        "Mocha: failed to load `file` module",
+        { cause: e },
+      );
+    })
+    .finally(() => console.log("Mocha: file: module loader executed")),
+  synthetic = new file(
     "Storage",
     {
       name: "SYNTHETIC_FILENAME.txt",
