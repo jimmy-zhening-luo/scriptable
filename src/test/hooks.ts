b5e485fb37006b1a0c28b4d3c51cd419229a1170
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-04-01 00:44:22 -0700

    try out global fixtures

--- src/test/hooks.ts
+++ src/test/hooks.ts
@@ -1,16 +1,15 @@
 import * as Synthetics from "./synthetics";
 
-export const mochaHooks = {
-  beforeAll() {
-    try {
-      console.log("Mocha: global setup hook begin");
-      global.args = Synthetics.args;
-      global.FileManager = Synthetics.FileManager;
-      global.Notification = Synthetics.Notification;
-      global.DateFormatter = Synthetics.DateFormatter;
-      console.log("Mocha: global setup hook end");
-    } catch (e) {
-      throw new EvalError("Mocha: failed global setup", { cause: e });
-    }
-  },
-};
+export function mochaGlobalTeardown() {
+  try {
+    console.log("Mocha: global setup hook begin");
+    global.args = Synthetics.args;
+    global.FileManager = Synthetics.FileManager;
+    global.Notification = Synthetics.Notification;
+    global.DateFormatter = Synthetics.DateFormatter;
+    console.log("Mocha: global setup hook end");
+  }
+  catch (e) {
+    throw new EvalError("Mocha: failed global setup", { cause: e });
+  }
+}
