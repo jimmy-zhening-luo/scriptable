008ade29c93d2d61d6200137a47372fbf43d713e
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-19 22:03:03 -0700

    widget members; create basic text widget

--- src/MyWidget.ts
+++ src/MyWidget.ts
@@ -3,6 +3,9 @@ import Widget from "./app/widget";
 
 class MyWidget extends Widget {
   protected runtime() {
+    this.widget.addText("Testing");
+    this.widget.addText(this.input ?? "no input");
+
     return null;
   }
 }
