94b8eba55011eb1cf5e641432321cd8fcd9b87d8
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-19 22:15:50 -0700

    lint

--- src/app/widget.ts
+++ src/app/widget.ts
@@ -2,14 +2,14 @@ import App from "./base";
 
 export default abstract class Widget<Schema = never> extends App<string, null, Schema> {
   protected readonly widget = new ListWidget();
-  
+
   protected getInput() {
     return args.widgetParameter as Undef<string>;
   }
 
   protected output(runtime: ReturnType<Widget["runtime"]>) {
     Script.setWidget(this.widget);
-    console.log("Widget set")
+    console.log("Widget set");
 
     return runtime;
   }
