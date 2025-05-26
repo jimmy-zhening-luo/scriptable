ace3257aa6979a28a554a266e99ce129f1d23c34
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-25 21:22:19 -0700

    lint apps

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,19 +4,27 @@ export default abstract class Shortcut<
   Input = never,
   Output = null,
   Schema = never,
-> extends IApp<Input, Null<Output>, Schema> {
+> extends IApp<
+  Input,
+  Null<Output>,
+  Schema
+> {
   protected getInput() {
     return this.stringInput === true
       ? args.plainTexts[0] as unknown as Undef<Input>
       : args.shortcutParameter as Undef<Input>;
   }
 
-  protected output(runtime: ReturnType<Shortcut<Input, Output>["runtime"]>) {
+  protected output(
+    runtime: ReturnType<Shortcut<Input, Output>["runtime"]>,
+  ) {
     console.log(runtime);
     Script.setShortcutOutput(runtime);
 
     return runtime;
   }
 
-  protected stringInput?: Input extends string ? boolean : never;
+  protected stringInput?: Input extends string
+    ? boolean
+    : never;
 }
