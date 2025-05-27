89a384b2b0526ede30bba57f5eb10c2cc98daac4
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-27 00:32:20 -0700

    lint

--- src/_test/hooks.ts
+++ src/_test/hooks.ts
@@ -56,7 +56,7 @@ export async function mochaGlobalSetup() {
     global.MockConcreteShortcut = class MockConcreteShortcut extends MockShortcut<
       string,
       string
-      > {
+    > {
       protected override stringInput = true;
 
       protected runtime() {
