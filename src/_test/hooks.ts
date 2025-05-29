bc2b20f9d0383d6482bf5334b6993a003897c78e
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-28 19:02:48 -0700

    update hooks.ts for shortcut input type

--- src/_test/hooks.ts
+++ src/_test/hooks.ts
@@ -61,8 +61,6 @@ export async function mochaGlobalSetup() {
       string,
       string
     > {
-      protected override stringInput = true;
-
       protected runtime() {
         return "CONCRETE_SHORTCUT_OUTPUT";
       }
