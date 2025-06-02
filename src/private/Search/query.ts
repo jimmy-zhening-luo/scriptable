83e51fa33ae86e1ae5710351a502e6300d88f5d1
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-06-01 22:48:54 -0700

    wrap query parser in parse

--- src/private/Search/query.ts
+++ src/private/Search/query.ts
@@ -6,117 +6,131 @@ export default function (
   OPERATORS: stringful,
   SELECTORS: Arrayful<char>,
 ) {
-  function select(
+  function parse(
     query: string,
     FALLBACKS: Arrayful<stringful>,
     OPERATORS: stringful,
     SELECTORS: Arrayful<char>,
   ) {
-    function expand(
+    function select(
       query: string,
       FALLBACKS: Arrayful<stringful>,
       OPERATORS: stringful,
+      SELECTORS: Arrayful<char>,
     ) {
-      function tokenize(
+      function expand(
         query: string,
         FALLBACKS: Arrayful<stringful>,
+        OPERATORS: stringful,
       ) {
-        const tokens = query
-          .split(" ")
-          .filter((token): token is stringful => token !== ""),
-        spaces = query.length - query.trimStart().length;
-
-        if (spaces > 0)
-          tokens.unshift(
-            FALLBACKS.at(
-              Math.min(
-                spaces,
-                FALLBACKS.length,
-              ) - 1,
-            )!,
-          );
-
-        if (tokens.length === 0)
-          throw new RangeError(
-            "Empty query",
-            { cause: { query } },
-          );
-
-        const [first, ...rest] = tokens;
-
+        function tokenize(
+          query: string,
+          FALLBACKS: Arrayful<stringful>,
+        ) {
+          const tokens = query
+            .split(" ")
+            .filter((token): token is stringful => token !== ""),
+          spaces = query.length - query.trimStart().length;
+  
+          if (spaces > 0)
+            tokens.unshift(
+              FALLBACKS.at(
+                Math.min(
+                  spaces,
+                  FALLBACKS.length,
+                ) - 1,
+              )!,
+            );
+  
+          if (tokens.length === 0)
+            throw new RangeError(
+              "Empty query",
+              { cause: { query } },
+            );
+  
+          const [first, ...rest] = tokens;
+  
+          return [
+            first as stringful,
+            ...rest,
+          ] as const;
+        }
+  
+        const tokens = tokenize(
+          query,
+          FALLBACKS,
+        ),
+        [[token0_0, token0_1]] = tokens;
+  
         return [
-          first as stringful,
-          ...rest,
+          ...token0_0 >= "0"
+          && token0_0 <= "9"
+          || OPERATORS.includes(token0_0)
+          || typeof token0_1 !== "undefined"
+          && Number.isFinite(
+            Number(
+              [
+                token0_0,
+                token0_1,
+              ].join(""),
+            ),
+          )
+            ? ["math" as stringful] as const
+            : [] as const,
+          ...tokens,
         ] as const;
       }
-
-      const tokens = tokenize(
+  
+      for (const selector of SELECTORS)
+        if (OPERATORS.includes(selector))
+          throw new SyntaxError("Operators include reserved selector");
+  
+      const tokens = expand(
         query,
         FALLBACKS,
+        OPERATORS,
       ),
-      [[token0_0, token0_1]] = tokens;
-
-      return [
-        ...token0_0 >= "0"
-        && token0_0 <= "9"
-        || OPERATORS.includes(token0_0)
-        || typeof token0_1 !== "undefined"
-        && Number.isFinite(
-          Number(
-            [
-              token0_0,
-              token0_1,
-            ].join(""),
-          ),
-        )
-          ? ["math" as stringful] as const
-          : [] as const,
-        ...tokens,
-      ] as const;
+      [head, ...terms] = tokens,
+      selector = SELECTORS
+        .find(
+          selector => head.includes(selector),
+        );
+  
+      if (typeof selector === "undefined")
+        return tokens;
+      else {
+        const [newHead = "", ...parts] = head
+          .split(selector),
+        key = newHead === ""
+          ? "translate" as stringful
+          : newHead as stringful,
+        selection = [
+          SELECTORS[0],
+          selector === "."
+          && parts.length === 1
+          && parts.at(0) === ""
+            ? terms.shift()
+            ?? ""
+            : parts.join(selector),
+        ].join("") as stringful;
+  
+        return [
+          key,
+          selection,
+          ...terms,
+        ] as const;
+      }
     }
 
-    for (const selector of SELECTORS)
-      if (OPERATORS.includes(selector))
-        throw new SyntaxError("Operators include reserved selector");
-
-    const tokens = expand(
+    return select(
       query,
       FALLBACKS,
       OPERATORS,
-    ),
-    [head, ...terms] = tokens,
-    selector = SELECTORS
-      .find(
-        selector => head.includes(selector),
-      );
-
-    if (typeof selector === "undefined")
-      return tokens;
-    else {
-      const [newHead = "", ...parts] = head
-        .split(selector),
-      key = newHead === ""
-        ? "translate" as stringful
-        : newHead as stringful,
-      selection = [
-        SELECTORS[0],
-        selector === "."
-        && parts.length === 1
-        && parts.at(0) === ""
-          ? terms.shift()
-          ?? ""
-          : parts.join(selector),
-      ].join("") as stringful;
-
-      return [
-        key,
-        selection,
-        ...terms,
-      ] as const;
-    }
+      SELECTORS,
+    );
   }
 
-  const [Head, ...rest] = select(
+  const [Head, ...rest] = parse(
     query,
     FALLBACKS,
     OPERATORS,
