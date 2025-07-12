9dba328b8b0660b3c871c1add47c1d7a602c151f
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-12 10:00:07 -0700

    Update line method call and add sunset field display

--- src/Clock.ts
+++ src/Clock.ts
@@ -4,12 +4,16 @@ import Widget from "./core/widget";
 class Clock extends Widget {
   protected runtime() {
     this.clock();
-    this.line();
+    this.line(5);
     this.field("China");
     this.clock(
       {
         timezone: "Asia/Shanghai",
       },
+    );
+    this.line();
+    this.field(
+      "Sunset: " + this.feed("sunset.txt"),
     );
   }
 }
