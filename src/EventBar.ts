c3b45060e6537cdc66d54214c1a9fd738cb21f85
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-13 17:49:33 -0700

    Add EventBar component to the project

---
+++ src/EventBar.ts
@@ -0,0 +1,13 @@
+// icon-color: cyan; icon-glyph: calendar-alt;
+import Widget from "./core/widget";
+
+class EventBar extends Widget {
+  protected runtime() {
+    this.line();
+  }
+}
+
+new EventBar(
+  null,
+  false,
+).run();
