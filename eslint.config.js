21f06a7ac0e0e9bb08cb243958f5f0a9072df3b5
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-02 10:09:08 -0700

    lint test

--- eslint.config.js
+++ eslint.config.js
@@ -5,5 +5,18 @@ export default linted(
     "*": {
       ignores: ["typings/scriptable.d.ts"],
     },
+    js: {
+      rules: {
+        "@stylistic/no-multiple-empty-lines": [
+          ON,
+          {
+            max: 1,
+            maxEOF: 0,
+            maxBOF: 0,
+          },
+        ],
+      }
+    },
   },
 );
+
