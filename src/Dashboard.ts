bf8b7181608cc4c697362aad250d018d03115132
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-06 16:08:51 -0700

    Update Dashboard runtime with countdown timer feature

--- src/Dashboard.ts
+++ src/Dashboard.ts
@@ -3,7 +3,25 @@ import Widget from "./core/widget";
 
 class Dashboard extends Widget {
   protected runtime() {
-    this.text("Hello, Dashboard.");
+    this
+      .text("Finish")
+      centerAlignText();
+
+    const timer = this.widget.addStack();
+
+    timer.spacing = 0;
+    timer.centerAlignContent();
+
+    const countdown = timer.addDate(
+      (new Widget.Time)
+        .at(this.input)
+        .in(8)
+        .toDate(),
+    );
+
+    countdown.font = this.style.round.regular(16);
+    countdown.centerAlignText();
+    countdown.applyTimerStyle();
   }
 }
 
