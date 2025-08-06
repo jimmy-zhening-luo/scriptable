9fa7cb94f221ebefb562cd001398e74905837a1a
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-06 16:51:08 -0700

    Refactor runtime method in Dashboard class

--- src/Dashboard.ts
+++ src/Dashboard.ts
@@ -3,25 +3,33 @@ import Widget from "./core/widget";
 
 class Dashboard extends Widget {
   protected runtime() {
-    this
-      .text("Finish")
-      .centerAlignText();
+    const { input } = this;
 
-    const timer = this.widget.addStack();
-
-    timer.spacing = 0;
-    timer.centerAlignContent();
-
-    const countdown = timer.addDate(
-      (new Widget.Time)
+    if (typeof input !== "string" || input === "")
+      this.text("—").centerAlignText();
+    else {
+      const finish = (new Widget.Time)
         .at(this.input)
         .in(8)
-        .toDate(),
-    );
+        .toDate();
 
-    countdown.font = this.style.round.regular(16);
-    countdown.centerAlignText();
-    countdown.applyTimerStyle();
+      if (new Widget.Time > finish)
+        this.text("Done").centerAlignText();
+      else {
+        this.text("Remaining").centerAlignText();
+
+        const timer = this.widget.addStack();
+
+        timer.spacing = 0;
+        timer.centerAlignContent();
+    
+        const countdown = timer.addDate(finish);
+
+        countdown.font = this.style.round.regular(16);
+        countdown.centerAlignText();
+        countdown.applyTimerStyle();
+      }
+    }
   }
 }
 
