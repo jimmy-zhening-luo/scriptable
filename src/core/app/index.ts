99aed174972968f8f2f9a1453eb44998784a639d
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-12 22:38:10 -0700

    Refactor string concatenation in index.ts

--- src/core/app/index.ts
+++ src/core/app/index.ts
@@ -45,7 +45,7 @@ export default abstract class IApp<
       return this._setting ??= JSON.parse(
         new File(
           "Setting",
-          `${this.app}.json`,
+          this.app + ".json",
         )
           .readStringful(),
       ) as Setting;
