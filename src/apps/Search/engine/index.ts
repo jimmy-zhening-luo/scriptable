00eb20aac1339b94d7541c69bcb47d806f2f98f7
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-12 22:30:33 -0700

    Refactor browserOptions type definition in index.ts

--- src/apps/Search/engine/index.ts
+++ src/apps/Search/engine/index.ts
@@ -17,8 +17,8 @@ export default function<EngineType extends "browser" | "shortcut">(
     terms: readonly stringful[],
     separator: string,
     browserOptions?: {
-      urls: Undefined<readonly string[]>;
-      tag: Undefined<stringful>;
+      urls: Null<readonly string[]>;
+      tag: Null<stringful>;
       force: boolean;
     },
   ) {
