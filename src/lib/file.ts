67d447e4cae478f4dc041b2b4706db4aac7b8548
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-09-21 19:22:40 -0700

    file: tri-state

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -3,8 +3,10 @@ export default class File<Class extends string> {
   private readonly path;
   private readonly parent;
   private readonly mutable;
-  private readonly exists;
-  private readonly isFolder;
+  private state: Null<
+    | "Folder"
+    | "File"
+  > = null;
 
   constructor(
     Class: Literalful<Class>,
@@ -17,9 +19,9 @@ export default class File<Class extends string> {
     } = {},
   ) {
     if (hidden && !mutable)
-      throw new TypeError(
-        "Hidden file must be mutable",
-        { cause: Class },
+      this.error(
+        "construct",
+        Class.concat(": Hidden file must be mutable"),
       );
 
     const root = [
@@ -44,9 +46,9 @@ export default class File<Class extends string> {
       .filter(node => node !== "");
 
     if (subpath.length === 0)
-      throw new ReferenceError(
-        "File has empty subpath",
-        { cause: `<${Class}>${subfolder}/${name}` },
+      this.error(
+        "construct",
+        Class.concat(": File has empty subpath"),
       );
 
     this.path = [...root, ...subpath]
@@ -57,32 +59,34 @@ export default class File<Class extends string> {
     ]
       .join("/");
     this.mutable = mutable;
-    this.exists = File.manager.fileExists(this.path);
-    this.isFolder = File.manager.isDirectory(this.path);
+
+    if (File.manager.fileExists(this.path))
+      this.state = File.manager.isDirectory(this.path)
+        ? "Folder"
+        : "File";
   }
 
   public read(fail = false) {
-    if (this.exists) {
-      if (this.isFolder)
-        throw this.error(
-          "read",
-          new TypeError("Target is Folder"),
-        );
-    }
-    else {
+    if (this.state === "File")
+      return File.manager.readString(this.path);
+    else if (this.state === null) {
       if (fail)
         throw this.error(
           "read",
-          new ReferenceError("File does not exist"),
+          "File does not exist",
+          "Reference",
         );
 
       return undefined;
     }
-
-    return File.manager.readString(this.path);
+    else
+      throw this.error(
+        "read",
+        "Target is folder",
+      );
   }
 
-  public readString(fail?: boolean): string {
+  public readString(fail?: boolean) {
     return this.read(fail) ?? "";
   }
 
@@ -92,18 +96,18 @@ export default class File<Class extends string> {
     if (content === "")
       throw this.error(
         "readStringful",
-        new TypeError("File is empty"),
+        "File is empty",
       );
 
     return content as stringful;
   }
 
   public write(
-    content?:
+    content:
       | string
       | number
       | boolean
-      | Record<string, unknown>,
+      | Record<string, unknown> = "",
     overwrite:
       | "line"
       | "append"
@@ -112,40 +116,45 @@ export default class File<Class extends string> {
     if (!this.mutable)
       throw this.error(
         "write",
-        new TypeError("Readonly"),
+        "Readonly",
       );
-    else if (this.isFolder)
-      throw this.error(
-        "write",
-        new TypeError("Target is Folder"),
-      );
-    else if (this.exists) {
+
+    if (this.state === "File") {
       if (overwrite === false)
         throw this.error(
           "write",
-          new ReferenceError("File exists and overwrite:false"),
+          "File exists, but overwrite mode false",
+          "Reference",
         );
     }
-    else if (!File.manager.isDirectory(this.parent))
-      File.manager.createDirectory(this.parent, true);
-
-    const buffer = content ?? "";
+    else if (this.state === null) {
+      if (!File.manager.isDirectory(this.parent))
+        File.manager.createDirectory(
+          this.parent,
+          true,
+        );
+    }
+    else
+      throw this.error(
+        "write",
+        "Target is folder",
+      );
 
     File.manager.writeString(
       this.path,
-      typeof buffer === "object"
-        ? JSON.stringify(buffer)
-        : typeof overwrite !== "string"
-          ? String(buffer)
+      typeof content === "object"
+        ? JSON.stringify(content)
+        : typeof overwrite === "boolean" || this.state === null
+          ? String(content)
           : overwrite === "line"
-            ? String(buffer)
+            ? String(content)
                 .concat(
                   "\n",
                   this.readString(),
                 )
             : this
                 .readString()
-                .concat(buffer as string),
+                .concat(content as string),
     );
   }
 
@@ -153,18 +162,33 @@ export default class File<Class extends string> {
     if (!this.mutable)
       throw this.error(
         "delete",
-        new ReferenceError("Readonly"),
+        "Readonly",
       );
 
-    if (this.exists)
+    if (this.state !== null) {
       File.manager.remove(this.path);
+      this.state = null;
+    }
   }
 
-  private error(verb: string, error: unknown) {
+  private error(
+    verb: string,
+    message: string,
+    error:
+      | "Reference"
+      | "Type" = "Type",
+  ) {
     return new Error(
       `Failed to ${verb} file`,
       {
-        cause: new Error(this.path, { cause: error }),
+        cause: new Error(
+          this.path,
+          {
+            cause: error === "Type"
+              ? new TypeError(message)
+              : new ReferenceError(message),
+          },
+        ),
       },
     );
   }
