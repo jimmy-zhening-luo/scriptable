358413305f652a8f6279f728bc1ceb905a60309a
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-09-25 19:37:50 -0700

    Refactor rollup.config.js by removing noShake function

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,7 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { babel } from "@rollup/plugin-babel";
 
-const extensions = [".ts"],
-noShake = () => ({
-  name: "no-treeshake",
-  resolveId(id, importer) {
-    return !importer
-      ? {
-          id,
-          moduleSideEffects: "no-treeshake",
-        }
-      : null;
-  },
-});
+const extensions = [".ts"];
 
 export default {
   input: "./src/core/index.ts",
