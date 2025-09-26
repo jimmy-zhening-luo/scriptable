3635cd026bdb5801150778436d749ea9b8001018
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-09-25 18:52:38 -0700

    test rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { babel } from "@rollup/plugin-babel";
+
+const extensions = [".ts"],
+noShake = () => ({
+  name: "no-treeshake",
+  resolveId(id, importer) {
+    return !importer
+      ? {
+          id,
+          moduleSideEffects: "no-treeshake",
+        }
+      : null;
+  },
+});
+
+export default {
+  input: "./src/core/index.ts",
+  output: {
+    file: "./dist/core/index.js",
+  },
+  plugins: [
+    noShake(),
+    nodeResolve({ extensions }),
+    babel({
+      extensions,
+      babelHelpers: "runtime",
+    }),
+  ],
+};
