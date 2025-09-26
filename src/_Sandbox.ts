f490143e7ff9d09482fa386314170865fe05425b
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-09-25 21:23:03 -0700

    lint

--- src/_Sandbox.ts
+++ src/_Sandbox.ts
@@ -1,11 +1,11 @@
 // icon-color: deep-green; icon-glyph: play;
 import Shortcut from "./app";
 
-await new (class Sandbox extends Shortcut {
+await new class Sandbox extends Shortcut {
   /* eslint-disable @typescript-eslint/require-await */
   protected async runtime() {
     const foo = null;
 
     return foo;
   }
-})().run();
+}().run();
