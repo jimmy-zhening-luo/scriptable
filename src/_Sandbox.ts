2bcbcd8dd9f2f28000d15bc13fda24987d7ca6ad
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-09-25 21:22:18 -0700

    try IICE idiom

--- src/_Sandbox.ts
+++ src/_Sandbox.ts
@@ -1,13 +1,11 @@
 // icon-color: deep-green; icon-glyph: play;
 import Shortcut from "./app";
 
-class Sandbox extends Shortcut {
+await new (class Sandbox extends Shortcut {
   /* eslint-disable @typescript-eslint/require-await */
   protected async runtime() {
     const foo = null;
 
     return foo;
   }
-}
-
-await new Sandbox().run();
+})().run();
