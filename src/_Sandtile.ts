1041d877bec5ede542e6b50cc7591b9a13ad21fb
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-11 18:33:16 -0700

    null

--- src/_Sandtile.ts
+++ src/_Sandtile.ts
@@ -4,6 +4,6 @@ import Widget from "./app/widget";
 await new class Sandtile extends Widget {
   /* eslint-disable @typescript-eslint/require-await */
   protected async runtime() {
-    void this.text(this.app);
+    void this.text(null);
   }
 }().run();
