9923afbcf20f6ad35e966fde5d6f5c54e5e92cc0
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-15 14:56:48 -0700

    Refactor Timer runtime logic and add IWhen interface

--- src/Timer.ts
+++ src/Timer.ts
@@ -1,23 +1,54 @@
 // icon-color: teal; icon-glyph: hourglass-half;
 import Widget from "./app/widget";
 
+interface IWhen {
+  when: string;
+}
+
 void new class Timer extends Widget {
   protected runtime() {
-    const feed = this.subscribe("when", "json"),
-    { when } = feed === undefined ? {} : JSON.parse(feed) as { when?: string },
-    time = when === undefined ? null : new Timer.Time(when),
-    now = new Timer.Time;
+    function extract(data?: string) {
+      return data === undefined
+        ? null
+        : new Timer.Time(
+            (JSON.parse(data) as IWhen)
+              .when,
+          );
+    }
 
-    if (
-      time === null
-      || time < now.midnight
-      || time > now
-    )
+    const store = extract(
+      this.read(),
+    ),
+    feed = extract(
+      this.subscribe("when", "json"),
+    ),
+    {
+      when = null,
+      update = false,
+    } = store !== null && store.today
+      ? {
+          when: store,
+        }
+      : feed !== null && feed.today
+        ? {
+            when: feed,
+            update: true,
+          }
+        : {};
+
+    if (when === null)
       this.text("+").centerAlignText();
     else {
-      const end = time.in(8);
+      if (update)
+        this.write(
+          {
+            when: when.print(),
+          },
+        );
 
-      if (now > end)
+      const end = when.in(8);
+
+      if (new Timer.Time > end)
         this.text("\u2713").centerAlignText();
       else {
         this.text("Remaining").centerAlignText();
