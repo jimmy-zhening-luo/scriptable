29d0c67a69110fb875f6f445b59e7d2524ab0105
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-15 20:54:46 -0700

    Add finiteful and integerful type definitions for enhanced type safety

--- typings/utility/primitive/assertion/number/finite.d.ts
+++ typings/utility/primitive/assertion/number/finite.d.ts
@@ -2,3 +2,4 @@ declare const finite: unique symbol;
 declare type finite = number & {
   [finite]: true;
 };
+declare type finiteful = finite & numberful;
--- typings/utility/primitive/assertion/number/integer.d.ts
+++ typings/utility/primitive/assertion/number/integer.d.ts
@@ -2,3 +2,4 @@ declare const integer: unique symbol;
 declare type integer = finite & {
   [integer]: true;
 };
+declare type integerful = integer & numberful;
