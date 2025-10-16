7c30ec7f8661dd2a814599a216fc1ff43b3595d0
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-15 21:02:19 -0700

    Fix default type parameter in polar type definition to include 0

--- typings/utility/primitive/assertion/number/polarity.d.ts
+++ typings/utility/primitive/assertion/number/polarity.d.ts
@@ -1,4 +1,4 @@
 declare const polar: unique symbol;
-declare type polar<Polarity extends 0 | "-" | "+" = "+"> = number & {
+declare type polar<Polarity extends 0 | "-" | "+" = 0 | "+"> = number & {
   [polar]: Polarity;
 };
