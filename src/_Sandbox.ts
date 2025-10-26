67ff2d6f006af390b93db6e961cf2d2268e4c0e4
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-26 03:02:10 -0700

    Remove unused import statement in _Sandbox.ts

--- src/_Sandbox.ts
+++ src/_Sandbox.ts
@@ -1,5 +1,6 @@
 // icon-color: deep-green; icon-glyph: play;
 import Shortcut from "./app";
+// import Time from "./lib/time";
 
 await new class Sandbox extends Shortcut {
   protected async runtime() {
