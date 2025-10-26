e849aa9388e390077320b14a77bd0a5a703e5245
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-26 03:00:22 -0700

    Remove unused Time import from _Sandbox.ts

--- src/_Sandbox.ts
+++ src/_Sandbox.ts
@@ -1,6 +1,5 @@
 // icon-color: deep-green; icon-glyph: play;
 import Shortcut from "./app";
-import Time from "./lib/time";
 
 await new class Sandbox extends Shortcut {
   protected async runtime() {
