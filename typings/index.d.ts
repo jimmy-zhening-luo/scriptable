c3e5a1f44b0b66f0cb5ca2a8a2c6cafa9ce9ce10
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-27 15:34:55 -0700

    Refactor nullish type and add falsy type definition
    - Removed nullish type from index.d.ts
    - Added nullish and falsy type definitions in null.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,4 @@
-declare type nullish = null | undefined;
-
 declare type primitive
 = | string
   | number
-  | boolean
-    ;
+  | boolean;
---
+++ typings/null.d.ts
@@ -0,0 +1,12 @@
+declare type nullish
+= | null
+  | undefined;
+
+declare type falsy
+= | nullish
+  | false
+  // | NaN
+  | 0
+  | -0
+  | 0n
+  | "";
