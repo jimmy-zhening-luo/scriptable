778e2d8c2470a27a36a5fad9c4706824a04a8dc5
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-28 17:54:28 -0700

    Add FieldfulTable type to tables.d.ts

--- typings/utility/record/alias/tables.d.ts
+++ typings/utility/record/alias/tables.d.ts
@@ -1,4 +1,5 @@
 declare type FieldTable = Table<string>;
+declare type FieldfulTable = Table<stringful>;
 declare type FlagTable = Table<boolean>;
 declare type ScalarTable = Table<number>;
 declare type ListTable = Table<readonly string[]>;
