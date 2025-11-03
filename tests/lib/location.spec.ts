0a7abfbedfa343eda46250f46e6aa974f8638a33
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-03 00:36:14 -0800

    Add tests for location functionality in spec file

---
+++ tests/lib/location.spec.ts
@@ -0,0 +1,74 @@
+import "chai/register-should.js";
+import location from "../../lib/location";
+
+const coordinates = ["latitude", "longitude"];
+
+describe(
+  "Time",
+  function () {
+    describe(
+      "shape",
+      function () {
+        it(
+          "is a function",
+          function () {
+            location
+              .should.be
+              .a("function");
+          },
+        );
+        it(
+          "is callable",
+          function () {
+            location
+              .should
+              .not
+              .throw();
+          },
+        );
+      },
+    );
+    describe(
+      "returns",
+      function () {
+        it(
+          "an object containing latlong",
+          async function (done) {
+            await location()
+              .should
+              .be
+              .an("object")
+              .with
+              .keys(coordinates);
+          },
+        );
+        it(
+          "strings",
+          async function (done) {
+            for (const coordinate of coordinates)
+              await location()
+                .should
+                .have
+                .own
+                .property(coordinate)
+                .a("string");
+          },
+        );
+        it(
+          "containing floats",
+          async function (done) {
+            for (const coordinate of coordinates)
+              parseFloat(
+                await location().latitude,
+              )
+                .should
+                .be
+                .a("number")
+                .not
+                .NaN;
+          },
+        );
+      },
+    );
+  },
+);
