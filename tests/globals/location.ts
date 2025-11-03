41d4436490def6d96a795fd4641363bb463927e9
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-02 21:10:39 -0800

    correct return type loc

--- tests/globals/location.ts
+++ tests/globals/location.ts
@@ -6,10 +6,42 @@ const enum Accuracy {
   Best,
 }
 
-const MOCK_LOCATION = {
+const MOCK_LATLONG = {
   latitude: 82.3048478,
   longitude: -150.2048384,
 };
+
+interface GeocodeSummary {
+  subAdministrativeArea: string | null;
+  postalAddress: {
+    country: string;
+    postalCode: string;
+    subAdministrativeArea: string;
+    subLocality: string;
+    state: string;
+    street: string;
+    city: string;
+    isoCountryCode: string;
+  };
+  isoCountryCode: string | null;
+  timeZone: string;
+  location: {
+    altitude: number;
+    longitude: number;
+    latitude: number;
+  };
+  country: string | null;
+  subThoroughfare: string | null;
+  thoroughfare: string | null;
+  name: string;
+  locality: string | null;
+  areasOfInterest: string[] | null;
+  ocean: string | null;
+  subLocality: string | null;
+  postalCode: string | null;
+  administrativeArea: string | null;
+  inlandWater: string | null;
+}
 
 /* eslint-disable ts/no-extraneous-class */
 export class Location {
@@ -19,23 +51,60 @@ export class Location {
     const {
       latitude,
       longitude,
-    } = MOCK_LOCATION,
+    } = MOCK_LATLONG,
     { accuracy } = Location;
 
     return Promise.resolve(
       {
         latitude: Number(latitude.toFixed(accuracy)),
         longitude: Number(longitude.toFixed(accuracy)),
+        altitude: 100,
+        verticalAccuracy: accuracy,
+        horizontalAccuracy: accuracy,
       },
     );
   }
 
-  public static reverseGeocode() {
-    return [
-      {
-        address: "123 Main St",
-      },
-    ];
+  public static async reverseGeocode(
+    latitude: number,
+    longitude: number,
+    locale = "en-US",
+  ): Promise<Array<GeocodeSummary>> {
+    return Promise.resolve(
+      [
+        {
+          subAdministrativeArea: "North Slope Borough",
+          postalAddress: {
+            country: "United States",
+            postalCode: "99723",
+            subAdministrativeArea: "North Slope Borough",
+            subLocality: "Utqiaġvik",
+            state: "Alaska",
+            street: "123 Example St",
+            city: "Utqiaġvik",
+            isoCountryCode: "US",
+          },
+          isoCountryCode: "US",
+          timeZone: "America/Anchorage",
+          location: {
+            altitude: 100,
+            longitude,
+            latitude,
+          },
+          country: "United States",
+          subThoroughfare: "123",
+          thoroughfare: "Example St",
+          name: "Example Place",
+          locality: "Utqiaġvik",
+          areasOfInterest: ["Example Area " + locale],
+          ocean: null,
+          subLocality: "Utqiaġvik",
+          postalCode: "99723",
+          administrativeArea: "Alaska",
+          inlandWater: null,
+        },
+      ],
+    );
   }
 
   public static setAccuracyToBest() {
