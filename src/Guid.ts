ad70d32e910397b60a46c83d9217ae6cfaa14120
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-10 15:31:38 -0800

    Refactor Guid class development property

--- src/Guid.ts
+++ src/Guid.ts
@@ -2,8 +2,9 @@
 import Shortcut from "./app";
 
 void new class Guid extends Shortcut<never, string> {
-  protected override development = (guid: string) => {
-    Pasteboard.copy(guid);
+  protected override development = (guid: Void<string>) => {
+    if (guid !== undefined)
+      Pasteboard.copy(guid);
   };
 
   protected runtime() {
