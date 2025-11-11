3fe8d4059a305271b89eedb01cdcdae5b37f6134
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-10 20:01:46 -0800

    Refactor Guid class implementation in TypeScript

--- src/Guid.ts
+++ src/Guid.ts
@@ -3,8 +3,8 @@ import Shortcut from "./app";
 
 void new class Guid extends Shortcut<never, string> {
   protected override ui = (guid: Void<string>) => {
-    if (guid !== undefined)
-      Pasteboard.copy(guid);
+    Pasteboard.copy(guid!);
+    this.notify(guid);
   };
 
   protected runtime() {
