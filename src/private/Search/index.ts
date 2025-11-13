5faa78c1678168415e48c45fa213191deac3ea06
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-12 18:42:43 -0800

    Fix type assertion for Special.Selector in parser function

--- src/private/Search/index.ts
+++ src/private/Search/index.ts
@@ -79,7 +79,7 @@ export function parser(
       term,
     ] = keyterm;
 
-    if (term === Special.Selector) {
+    if (term === Special.Selector as char) {
       if (query.length === 1)
         query[0] = setting.reserved.selector;
       else
