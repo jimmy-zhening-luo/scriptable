530efda30c082706bfe5e360e4c7c87f39d1cf23
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-02 15:07:05 -0800

    Refactor accuracy handling in location function

--- src/lib/location/index.ts
+++ src/lib/location/index.ts
@@ -3,32 +3,32 @@ import type { Accuracy } from "./types";
 export default async function (
   accuracy: Accuracy = 1e-1,
 ) {
-  const Accuracy = {
-    0: "Best",
-    1e-2: "TenMeters",
-    1e-1: "HundredMeters",
-    1: "Kilometer",
-    3: "ThreeKilometers",
-  } as const,
-  Digits = {
+  const Accuracy = (
+    {
+      0: "Best",
+      1e-2: "TenMeters",
+      1e-1: "HundredMeters",
+      1: "Kilometer",
+      3: "ThreeKilometers",
+    } as const
+  )[accuracy],
+  precision = {
     0: 5,
     1e-2: 4,
     1e-1: 3,
     1: 2,
     3: 2,
-  };
+  }[accuracy];
 
-  Location[
-    `setAccuracyTo${
-      Accuracy[accuracy]
-    }`
-  ]();
+  Location[`setAccuracyTo${Accuracy}`]();
 
-  const location = await Location.current(),
-  digits = Digits[accuracy];
+  const {
+    latitude,
+    longitude,
+  } = await Location.current();
 
   return {
-    latitude: location.latitude.toFixed(digits),
-    longitude: location.longitude.toFixed(digits),
+    latitude: latitude.toFixed(precision),
+    longitude: longitude.toFixed(precision),
   };
 }
