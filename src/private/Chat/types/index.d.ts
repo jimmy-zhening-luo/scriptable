20612c8e39d3f7f17a69c7155938208a80b47e58
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-04 00:24:59 -0800

    Fix export path for Output type in index.d.ts

--- src/private/Chat/types/index.d.ts
+++ src/private/Chat/types/index.d.ts
@@ -1,4 +1,4 @@
 export type { Setting } from "./setting";
 export type { Input } from "./input";
-export type { Output } from "./input";
+export type { Output } from "./output";
 export type { Response } from "./response";
