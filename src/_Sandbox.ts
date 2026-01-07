f32b8c41f343c654716ab72f7dfd60d08a736c23
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-06 21:07:20 -0800

    Refactor Sandbox class to instantiate Book before retrieving phone number

--- src/_Sandbox.ts
+++ src/_Sandbox.ts
@@ -1,12 +1,11 @@
 "deep-green play";
 import Shortcut from "./app";
-import _ from "./lib/contact";
-
-const Book = new _;
+import Book from "./lib/contact";
 
 await new class Sandbox extends Shortcut {
   protected async runtime() {
-    const phone = await Book.phone();
+    const book = new Book,
+    phone = await book.phone();
 
     log(phone);
   }
