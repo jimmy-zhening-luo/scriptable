57efa1d16521913e309f59599296798184df19d4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-07 05:38:36 -0800

    Remove unused Book import and related code

--- src/_Sandbox.ts
+++ src/_Sandbox.ts
@@ -1,12 +1,8 @@
 "deep-green play";
 import Shortcut from "./app";
-import Book from "./lib/contact";
 
 await new class Sandbox extends Shortcut {
   protected async runtime() {
-    const book = new Book,
-    phonebook = await book.phonebook();
-
-    console.log(phonebook.length);
+    //
   }
 }().run();
