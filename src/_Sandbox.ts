d56f83c2db74e0c06111031366bf44a21be24b01
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-06 20:58:40 -0800

    Initialize Contact instance in Sandbox class and log phone number

--- src/_Sandbox.ts
+++ src/_Sandbox.ts
@@ -1,8 +1,13 @@
 "deep-green play";
 import Shortcut from "./app";
+import Contact from "./lib/contact";
 
 await new class Sandbox extends Shortcut {
+  private readonly Contact = new Contact();
+
   protected async runtime() {
-    //
+    const phone = await this.Contact.phone();
+
+    log(phone);
   }
 }().run();
