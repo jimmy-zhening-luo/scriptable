16f4218f2caa659605b3387c55234241be597ee3
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-21 19:30:32 -0800

    Refactor global declarations and imports structure

--- tests/globals/index.ts
+++ tests/globals/index.ts
@@ -1,9 +1,21 @@
-export { args } from "./args";
-export { config } from "./config";
-export { Data } from "./data";
-export { DateFormatter } from "./date-formatter";
-export { FileManager } from "./file-manager";
-export { Image } from "./image";
-export { Location } from "./location";
-export { Notification } from "./notification";
-export { Size } from "./size";
+import { args } from "./args";
+import { config } from "./config";
+import { Data } from "./data";
+import { DateFormatter } from "./date-formatter";
+import { FileManager } from "./file-manager";
+import { Image } from "./image";
+import { Location } from "./location";
+import { Notification } from "./notification";
+import { Size } from "./size";
+
+export function installGlobals() {
+  global.args = args;
+  global.config = config;
+  global.Data = Data;
+  global.DateFormatter = DateFormatter;
+  global.FileManager = FileManager;
+  global.Image = Image;
+  global.Location = Location;
+  global.Notification = Notification;
+  global.Size = Size;
+}
--- tests/index.ts
+++ tests/index.ts
@@ -1,13 +1,1 @@
-import * as Mock from "./globals";
-
-export function mochaGlobalSetup() {
-  global.args = Mock.args;
-  global.config = Mock.config;
-  global.Data = Mock.Data;
-  global.DateFormatter = Mock.DateFormatter;
-  global.FileManager = Mock.FileManager;
-  global.Image = Mock.Image;
-  global.Location = Mock.Location;
-  global.Notification = Mock.Notification;
-  global.Size = Mock.Size;
-}
+export { installGlobals as mochaGlobalSetup } from "./globals";
