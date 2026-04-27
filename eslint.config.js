fd871f32de1b3598361ec444a4511df3d14a36b3
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-04-27 12:22:46 -0700

    Disable ts/no-unnecessary-type-assertion rule due to known bug

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import linted from "linted";
 export default linted({
   ts: {
     rules: {
+      "ts/no-unnecessary-type-assertion": 0 /* @BUG 8.59.0, 8.59.1, ... */
       "ts/require-array-sort-compare": 0,
       "ts/strict-boolean-expressions": 0,
     },
